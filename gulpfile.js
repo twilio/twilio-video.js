440e5fb5f070876526da9a766bea08429f9ae9a1
Mark Roberts <markandrusroberts@gmail.com>
2015-02-27 15:28:47 -0800

    Add futurehostile configuration

--- gulpfile.js
+++ gulpfile.js
@@ -111,6 +111,7 @@ gulp.task('lint', function() {
   return gulp.src('./lib/**.js')
     .pipe(jshint({
       evil: true,
+      futurehostile: true,
       // globalstrict: true,
       laxbreak: true,
       node: true,
