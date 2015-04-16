9463208fabdfbacc84f98cb7e62da511a2bc4547
Mark Roberts <markandrusroberts@gmail.com>
2015-04-16 11:20:47 -0700

    Improve code reuse between UserAgent & SIPJSUserAgent

--- test/spec/util.js
+++ test/spec/util.js
@@ -1,6 +1,21 @@
 'use strict';
 
+var assert = require('assert');
 var Q = require('q');
+var util = require('../../lib/util');
+var iceServers = require('../../test')['iceServers'];
+
+function test() {
+  describe('util', function() {
+    it('getStunServers', function() {
+      assert(util.getStunServers(iceServers));
+    });
+
+    it('getTurnServers', function() {
+      assert(util.getTurnServers(iceServers));
+    });
+  });
+}
 
 function waitAll(promisesOrDeferreds, done) {
   promisesOrDeferreds = promisesOrDeferreds.map(function(pOrD) {
@@ -11,4 +26,5 @@ function waitAll(promisesOrDeferreds, done) {
   }, done);
 }
 
+module.exports.test = test;
 module.exports.waitAll = waitAll;
