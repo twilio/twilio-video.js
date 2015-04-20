56f3819329f3f2f010f048a6587beebcb1ad1edd
Mark Roberts <markandrusroberts@gmail.com>
2015-04-20 10:43:39 -0700

    Rewriting SIPJSUserAgent tests

--- test/spec/sipjsuseragent.js
+++ test/spec/sipjsuseragent.js
@@ -98,103 +98,123 @@ describe('SIPJSUserAgent', function() {
   });
 
   var ua2Name = randomName();
-  var ua2 = new SIPJSUserAgent(getCapabilityToken(ua2Name), options);
+  var ua2 = new SIPJSUserAgent(getCapabilityToken(ua2Name), { 'wsServer': wsServer, 'debug': false });
 
-  describe('Receive incoming call', function() {
-    var ict = null;
-    var ist = null;
-    var ua1Dialog = null;
-    var ua2Dialog = null;
+  describe('ua2.invite(ua1Name)', function() {
+    var ua2Ict = null;
+    var ua1Ist = null;
 
-    before(function(done) {
-      ua2.register().then(function() {
-        done();
-      }, done);
-    });
-
-    it('ua2 registers...', function(done) {
-      ua2.register().then(function() { done(); }, done);
-    });
-
-    it('emits "invite"', function(done) {
-      ict = ua2.invite(ua1Name, inviteOptions);
-      ict.then(null, function(error) {
-        if (ist === null) {
-          done(new Error('InviteClientTransaction failed'));
-        }
-      });
-      ua1.once('invite', function(_ist) {
-        ist = _ist;
-        try {
-          assert.equal(ua1, ist.userAgent);
-          assert.equal(ua2Name, ist.from);
-          assert(ist.callSid);
-        } catch (e) {
-          return done(e);
-        }
+    before(function ua2CallsUA1(done) {
+      ua2Ict = ua2.invite(ua1Name, inviteOptions);
+      ua1.once('invite', function(ist) {
+        ua1Ist = ist;
         done();
       });
     });
 
-    it('updates .inviteServerTransactions', function() {
-      assert(ua1.inviteServerTransactions.has(ist));
+    it('ua1 emits "invite"', function() {
+      // Do nothing
     });
 
-    it('inviteServerTransaction.callSid', function() {
-      assert(ist.callSid);
+    it('updates ua1.inviteServerTransactions', function() {
+      assert(ua1.inviteServerTransactions.has(ua1Ist));
     });
 
-    it.skip('inviteServerTransaction.conversationSid', function() {
-      assert(ist.conversationSid);
+    it('updates ua2.inviteClientTransactions', function() {
+      assert(ua2.inviteClientTransactions.has(ua2Ict));
     });
 
-    describe('InviteServerTransaction#accept', function() {
-      it('updates .inviteServerTransactions', function(done) {
-        Q.all([
-          ict.then(function(_dialog) {
-            ua2Dialog = _dialog;
-          }),
-          ist.accept().then(function(_dialog) {
-            assert(!ua1.inviteServerTransactions.has(ist));
-            ua1Dialog = _dialog;
-          })
-        ]).then(function() {
-          done();
-        }, done);
+    describe('InviteServerTransaction', function() {
+      it('.callSid', function() {
+        assert(ua1Ist.callSid);
       });
 
-      it('updates .dialogs', function() {
-        assert(ua1.dialogs.has(ua1Dialog));
+      it('.conversationSid', function() {
+        assert(ua1Ist.conversationSid);
       });
 
-      it('dialog.callSid', function() {
-        assert(ua1Dialog.callSid);
+      it('.userAgent', function() {
+        assert.equal(ua1, ua1Ist.userAgent);
       });
 
-      it.skip('dialog.conversationSid', function() {
-        assert(ua1Dialog.conversationSid);
+      it('.from', function() {
+        assert.equal(ua2Name, ua1Ist.from);
       });
 
-      describe('Dialog "ended" event', function() {
-        it('updates .dialogs', function(done) {
+      describe('#accept', function() {
+        var ua1Dialog = null;
+        var ua2Dialog = null;
+
+        this.timeout(0);
+
+        before(function(done) {
           Q.all([
-            ua1Dialog.end().then(function() {
-              assert(!ua1.dialogs.has(ua1Dialog));
+            ua2Ict.then(function(dialog) {
+              ua2Dialog = dialog;
             }),
-            ua2Dialog.end().then(function() {
-              assert(!ua2.dialogs.has(ua2Dialog));
+            ua1Ist.accept().then(function(dialog) {
+              ua1Dialog = dialog;
             })
           ]).then(function() {
             done();
           }, done);
         });
+
+        it('updates ua1.inviteServerTransactions', function() {
+          assert(!ua1.inviteServerTransactions.has(ua1Ist));
+        });
+
+        it('updates ua2.inviteClientTransactions', function() {
+          assert(!ua2.inviteClientTransactions.has(ua2Ict));
+        });
+
+        it('updates .dialogs', function() {
+          assert(ua1.dialogs.has(ua1Dialog));
+        });
+
+        it('dialog.callSid', function() {
+          assert(ua1Dialog.callSid);
+        });
+
+        it('dialog.conversationSid', function() {
+          assert(ua1Dialog.conversationSid);
+        });
+
+        describe('Dialog "ended" event', function() {
+          it('updates .dialogs', function(done) {
+            ua1Dialog.end().then(function() {
+              assert(!ua1.dialogs.has(ua1Dialog));
+            }, done);
+            ua2Dialog.once('ended', function() {
+              try {
+                assert(!ua2.dialogs.has(ua2Dialog));
+              } catch(e) {
+                return done(e);
+              }
+              done();
+            });
+          });
+        });
       });
+
     });
 
+    /*
     describe('InviteServerTransaction#reject', function() {
+      var ua2Ict = null;
+      var ua1Ist = null;
+
       it('updates .inviteServerTransactions', function(done) {
-        var ict = ua2.invite(ua1Name, inviteOptions);
+        this.timeout(0);
+        ua2Ict = ua2.invite(ua1Name, inviteOptions);
+        ua2Ict.then(null, function(error) {
+          if (ua1Ist === null) {
+            return done(new Error('InviteClientTransaction failed'));
+          }
+          done();
+        });
         ua1.once('invite', function(ist) {
+          ua1Ist = ist;
           try {
             assert(ua1.inviteServerTransactions.has(ist));
           } catch (e) {
@@ -205,31 +225,56 @@ describe('SIPJSUserAgent', function() {
           }, function(ist) {
             assert(ist.rejected);
             assert(!ua1.inviteServerTransactions.has(ist));
-          }).then(done, done);
+          }).then(null, done);
+        });
+      });
+    });
+    */
+
+    describe('InviteServerTransaction#reject', function() {
+      it('updates .inviteServerTransactions', function(done) {
+        this.timeout(0);
+        var ua2Ict = ua2.invite(ua1Name, inviteOptions);
+        ua1.once('invite', function(ist) {
+          ua1Ist = ist;
+          try {
+            assert(ua1.inviteServerTransactions.has(ua1Ist));
+          } catch (e) {
+            return done(e);
+          }
+          ua1Ist.reject().then(done, function() {
+            assert(ua1Ist.rejected);
+            assert(!ua1.inviteServerTransactions.has(ua1Ist));
+            ua2Ict.then(done, function() {
+              done();
+            });
+          }).then(null, done);
         });
       });
     });
 
-    describe('InviteServerTransaction canceled', function() {
+    describe.skip('InviteServerTransaction canceled', function() {
       it('updates .inviteServerTransactions', function(done) {
-        var ict = ua2.invite(ua1Name, inviteOptions);
+        var ua2Ict = ua2.invite(ua1Name, inviteOptions);
         ua1.once('invite', function(ist) {
+          ua1Ist = ist;
           try {
-            assert(ua1.inviteServerTransactions.has(ist));
+            assert(ua1.inviteServerTransactions.has(ua1Ist));
           } catch (e) {
             return done(e);
           }
-          ict.cancel().then(function() {
-            throw new Error('InviteServerTransaction succeeded');
-          }, function(ist) {
-            assert(ist.canceled);
-            assert(!ua1.inviteServerTransactions.has(ist));
-          }).then(done, done);
+          ua2Ict.cancel().then(done, function() {
+            ua1Ist.then(done, function() {
+              assert(ua1Ist.canceled);
+              assert(!ua1.inviteServerTransactions.has(ua1Ist));
+            }).then(done, done);
+          });
         });
       });
     });
   });
 
+  /*
   describe('#invite', function() {
     var ict = null;
     var ist = null;
@@ -334,6 +379,7 @@ describe('SIPJSUserAgent', function() {
       });
     });
   });
+  */
 });
 
 function randomName() {
