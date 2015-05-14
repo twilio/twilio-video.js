fc0480373816ebd388f89c99048470d4d7325f1a
Ryan Rowland <rrowland@twilio.com>
2015-05-14 13:57:22 -0700

    Implemented ability to pass an array into Conversation#invite

--- test/spec/conversation.js
+++ test/spec/conversation.js
@@ -35,11 +35,13 @@ describe('Conversation (SIPJSUserAgent)', function() {
   var charlieName = randomName();
   var charlieToken = getToken(charlieName);
   var charlie = null;
+  var charlieDialogs = [];
 
   // Donald is a UserAgent.
   var donaldName = randomName();
   var donaldToken = getToken(donaldName);
   var donald = null;
+  var donaldDialogs = [];
 
   var conversation = null;
   var dialog = null;
@@ -49,51 +51,91 @@ describe('Conversation (SIPJSUserAgent)', function() {
   options['wsServer'] = wsServer;
   options['logLevel'] = 'off';
 
-  before(function setupConversaton(done) {
-    this.timeout(0);
-    alice = new Endpoint(aliceToken, options);
-    bob = new SIPJSUserAgent(bobToken, options);
-    bob.connect().then(function() {
-      return Q.all([alice.listen(), bob.register()]).then(function() {
-        bob.on('invite', function(ist) {
-          ist.accept().then(function(_dialog) {
-            dialog = _dialog;
+  describe('constructor', function() {
+    before(function setupEndpoint(done) {
+      this.timeout(10000);
+      alice = new Endpoint(aliceToken, options);
+      bob = new SIPJSUserAgent(bobToken, options);
+
+      Q.all([alice.listen(), bob.connect()])
+        .then(function() {
+          return bob.register();
+        }).then(function() {
+          bob.on('invite', function(ist) {
+            ist.accept().then(function(_dialog) { dialog = _dialog; });
           });
-        });
-        return alice.createConversation(bobName);
-      });
-    }).then(function(_conversation) {
-      conversation = _conversation;
+          return alice.createConversation(bobName);
+        }).then(function(_conversation) {
+          conversation = _conversation;
+        }).then(done, done);
+    });
+
+    it('should set the .sid property', function() {
+      assert(conversation.sid);
+    });
+
+    it('should set the .localMedia property', function() {
+      assert(conversation.localMedia);
+    });
+
+    it('should set the .participants property to Participant address', function() {
       var hasBob = false;
       conversation.participants.forEach(function(participant) {
         hasBob = hasBob || participant.address === bobName;
       });
       assert(hasBob);
       assert.equal(1, conversation.participants.size);
-    }).then(done, done);
+    });
   });
 
   describe('#invite', function() {
-    before(function setupCharlie(done) {
+    before(function setupEndpointsAndAgents(done) {
+      this.timeout(10000);
+      alice = new Endpoint(aliceToken, options);
+      bob = new SIPJSUserAgent(bobToken, options);
       charlie = new SIPJSUserAgent(charlieToken, options);
-      charlie.on('invite', function(invite) {
-        invite.accept();
+      donald = new SIPJSUserAgent(donaldToken, options);
+  
+      bob.on('invite', function(ist) {
+        ist.accept().then(function(_dialog) { dialog = _dialog; });
+      });
+      charlie.on('invite', function(ist) {
+        ist.accept().then(function(_dialog) {
+          charlieDialogs.push(_dialog);
+        });
+
+        ist.session.once('accepted', function() {
+          setTimeout(function() { ist.session.mediaHandler.emit('addStream'); });
+        });
       });
 
-      charlie.connect().then(function() {
-        return charlie.register();
-      }).then(function() { done(); }, done);
+      donald.on('invite', function(ist) {
+        ist.accept().then(function(_dialog) {
+          donaldDialog.push(_dialog);
+        });
+
+        ist.session.mediaHandler.emit('addStream');
+      });
+
+      Q.all([alice.listen(), bob.connect(), charlie.connect(), donald.connect()])
+        .then(function() {
+          return Q.all([bob.register(), charlie.register(), donald.register()]);
+        }).then(function() {
+          return alice.createConversation(bobName);
+        }).then(function(_conversation) {
+          conversation = _conversation;
+          assert.equal(1, conversation.participants.size);
+        }).then(done, done);
     });
 
-    before(function setupDonald(done) {
-      donald = new SIPJSUserAgent(donaldToken, options);
-      donald.on('invite', function(invite) {
-        invite.accept();
-      });
+    // TODO: Revisit this and make sure it's working after stale registrants are fixed
+    // server-side.
+    afterEach(function() {
+      charlieDialogs.forEach(function(dialog) { dialog.end(); });
+      donaldDialogs.forEach(function(dialog) { dialog.end(); });
 
-      donald.connect().then(function() {
-        return donald.register();
-      }).then(function() { done(); }, done);
+      charlieDialogs = [];
+      donaldDialogs = [];
     });
 
     it('should throw an exception if no participantAddress is passed', function() {
@@ -104,30 +146,34 @@ describe('Conversation (SIPJSUserAgent)', function() {
       assert.throws(conversation.invite.bind(conversation, charlie));
     });
 
-    it('should return a promise<Participant>', function(done) {
+    it('should return a Promise<Participant> for one address', function(done) {
       conversation.invite(charlieName)
         .then(
           function(participant) { assert.equal(participant.address, charlieName); },
           function() { assert.fail(null, null, 'promise was rejected'); })
         .then(done, done);
     });
-  });
 
-  it('.sid', function() {
-    assert(conversation.sid);
-  });
-
-  it('.participants contains Participant address', function() {
-    var hasBob = false;
-    conversation.participants.forEach(function(participant) {
-      hasBob = hasBob || participant.address === bobName;
+    it('should return an Array<Promise<Participant>> for one address in an array', function(done) {
+      conversation.invite([charlieName])[0]
+        .then(
+          function(participant) { assert.equal(participant.address, charlieName); },
+          function() { assert.fail(null, null, 'promise was rejected'); })
+        .then(done, done);
     });
-    assert(hasBob);
-    assert.equal(1, conversation.participants.size);
-  });
 
-  it('.localMedia', function() {
-    assert(conversation.localMedia);
+    it('should return an Array<Promise<Participant>> for multiple addresses in an array', function(done) {
+      this.timeout(10000);
+      Q.all(conversation.invite([charlieName, donaldName])).then(
+          function(participants) {
+            var names = participants.map(function(participant) {
+              return participant.address;
+            });
+            assert(names.indexOf(charlieName) !== -1);
+            assert(names.indexOf(donaldName) !== -1);
+          }, function() { assert.fail(null, null, 'promise was rejected'); })
+        .then(done, done);
+    });
   });
 });
 
