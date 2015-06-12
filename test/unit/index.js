58af77d7e5b37871d8a60f2897f41e8652483188
Mark Roberts <markandrusroberts@gmail.com>
2015-06-12 12:13:37 -0700

    Add XMLHttpRequest unit tests

--- test/unit/index.js
+++ test/unit/index.js
@@ -1,4 +1,7 @@
+'use strict';
+
 require('./spec/accesstoken');
 require('./spec/signaling/useragent');
 require('./spec/util');
 require('./spec/util/cancelablepromise');
+require('./spec/util/request');
