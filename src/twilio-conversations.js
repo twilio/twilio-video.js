f581af5c988e3b2e2145afbc367c54998a4b700d
karthikbr82 <karthik@callstats.io>
2016-02-15 16:58:44 +0200

    callstats basic api's integrated, reporterror, associatemst yet to done

--- src/twilio-conversations.js
+++ src/twilio-conversations.js
@@ -4,6 +4,19 @@
  */
 /* eslint strict:0 */
 (function(root) {
+  /* eslint "no-unused-vars": [2, {"vars": "all", "varsIgnorePattern": "a"}] */
+  var appId = '175240362';
+  var appSecret = 'Gvd126EUWQheaWQX9mwmeWIbzvs=';
+
+  /* global callstats io jsSHA */
+  /* eslint no-undef: 2 */
+  /* eslint new-cap: [2, {"newIsCap": false}] */
+  var callStats = new callstats(null, io, jsSHA);
+
+  function csInitCallback(err, msg) {
+    console.log('CallStats Initializing Status: err=' + err + 'msg=' + msg);
+  }
+
   var Conversations = require('./twilio-conversations-bundle');
   /* globals define */
   if (typeof define === 'function' && define.amd) {
