ede3d5a3e69baff05aa415e9f6e0d0ef6505b65b
Mark Andrus Roberts <mroberts@twilio.com>
2016-09-29 16:06:30 -0700

    Canceling may throw internally, causing us to not stop LocalMedia

--- test/unit/spec/client.js
+++ test/unit/spec/client.js
@@ -1,7 +1,40 @@
 'use strict';
 
-var Client = require('../../../lib/client');
+const assert = require('assert');
+const Client = require('../../../lib/client');
+const sinon = require('sinon');
+const token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiIsImN0eSI6InR3aWxpby1mcGE7dj0xIn0.eyJqdGkiOiJTSzY3NGIxODg2OWYxMWZhY2M2NjVhNjVmZDRkZGYyZjRmLTE0NzUxOTAzNDgiLCJncmFudHMiOnsiaWRlbnRpdHkiOiJhc2QiLCJydGMiOnsiY29uZmlndXJhdGlvbl9wcm9maWxlX3NpZCI6IlZTM2Y3NWUwZjE0ZTdjOGIyMDkzOGZjNTA5MmU4MmYyM2EifX0sImlhdCI6MTQ3NTE5MDM0OCwiZXhwIjoxNDc1MTkzOTQ4LCJpc3MiOiJTSzY3NGIxODg2OWYxMWZhY2M2NjVhNjVmZDRkZGYyZjRmIiwic3ViIjoiQUM5NmNjYzkwNDc1M2IzMzY0ZjI0MjExZThkOTc0NmE5MyJ9.N0UuZSblqb7MknNuiRkiEVVEdmztm5AdYIhQp7zU2PI';
 
-describe('Client', function() {
-  //it('should have unit tests', function() { });
+describe('Client', () => {
+  describe('#connect', () => {
+    describe('called without LocalMedia', () => {
+      it('acquires LocalMedia', () => {
+        const getLocalMedia = sinon.spy();
+        const client = new Client(token, {
+          getLocalMedia: getLocalMedia
+        });
+        client.connect();
+        assert(getLocalMedia.calledOnce);
+      });
+
+      describe('and then immediately canceled by calling .cancel()', () => {
+        it('calls .stop() on the LocalMedia', () => {
+          const localMedia = {
+            stop: sinon.spy()
+          };
+          const getLocalMedia = () => Promise.resolve(localMedia);
+          const client = new Client(token, {
+            getLocalMedia: getLocalMedia
+          });
+          const promise = client.connect();
+          promise.cancel();
+          return promise.then(() => {
+            throw new Error('Unexpected resolution');
+          }, () => {
+            assert(localMedia.stop.calledOnce);
+          });
+        });
+      });
+    });
+  });
 });
