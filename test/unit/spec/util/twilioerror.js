4c3c1cf317cc82a89f6ca42d951c83ffd1667a0e
mmalavalli <mmalavalli@twilio.com>
2016-12-06 14:28:44 -0800

    Refactoring TwilioError to accept error code. Refactoring the list of TwilioErrors.

--- test/unit/spec/util/twilioerror.js
+++ test/unit/spec/util/twilioerror.js
@@ -1,7 +1,38 @@
 'use strict';
 
+var assert = require('assert');
 var TwilioError = require('../../../../lib/util/twilioerror');
 
 describe('TwilioError', function() {
-  //it('should have unit tests', function() { });
+  describe('constructor', () => {
+    var error;
+
+    before(() => {
+      error = new TwilioError(1234);
+    });
+
+    it('should be an instance of Error', () => {
+      assert(error instanceof Error);
+    });
+
+    it('should set the name and code properties', () => {
+      assert.equal(error.name, 'TwilioError');
+      assert.equal(error.code, 1234);
+    });
+
+    it('should set the message property to empty', () => {
+      assert.equal(error.message, '');
+    });
+
+    it('should set the stack property to start with "TwilioError"', () => {
+      assert(/^TwilioError/.test(error.stack));
+    });
+
+    context('when message is provided', () => {
+      it('should set the message property', () => {
+        var errorWithMsg = new TwilioError(1234, 'some error message');
+        assert.equal(errorWithMsg.message, 'some error message');
+      });
+    });
+  });
 });
