286dad8a9be8a374a0888558358a7f087e0966b8
Manjesh Malavalli <mmalavalli@twilio.com>
2017-01-31 17:51:49 -0800

    JSDK-1094 Adding unit tests and implementing @mroberts' suggestions.

--- test/unit/spec/ecs.js
+++ test/unit/spec/ecs.js
@@ -5,6 +5,8 @@ var ECS = require('../../../lib/ecs');
 var request = require('../../../lib/request');
 var sinon = require('sinon');
 var twilio = require('twilio');
+var TwilioError = require('../../../lib/util/twilioerror');
+var AccessTokenInvalidError = require('../../../lib/util/twilio-video-errors').AccessTokenInvalidError;
 
 var fakeToken = 'a.b.c';
 
@@ -58,6 +60,57 @@ describe('ECS', function() {
           assert.deepEqual(config, { foo: 'bar', a: 123 });
         });
       });
+
+      context('when request() rejects the returned promise with an error code and message', function() {
+        it('should throw the appropriate TwilioError if present', function() {
+          request.post = function() {
+            return Promise.reject('{"code": 20101, "message": "Invalid Access Token"}');
+          };
+          return testGetConfigurationError(AccessTokenInvalidError, 20101, 'Invalid Access Token');
+        });
+
+        it('should throw a generic TwilioError if there is no constructor for the given error code', function() {
+          request.post = function() {
+            return Promise.reject('{"code": 12345, "message": "Generic error"}');
+          };
+          return testGetConfigurationError(TwilioError, 12345, 'Generic error');
+        });
+      });
+
+      context('when request() rejects the returned promise without an error code or message', function() {
+        it('should throw a generic TwilioError with error code 0 and message "Unknown error"', function() {
+          request.post = function() {
+            return Promise.reject('{"abc": 123, "def": "xyz"}');
+          };
+          return testGetConfigurationError(TwilioError, 0, 'Unknown error');
+        });
+      });
+
+      context('when request() rejects the returned promise with bad JSON response', function() {
+        it('should throw a generic TwilioError with error code 0', function() {
+          request.post = function() {
+            return Promise.reject('{"code": 123, "message" "xyz"}');
+          };
+          return testGetConfigurationError(TwilioError, 0);
+        });
+      });
     });
   });
 });
+
+function testGetConfigurationError(klass, code, message) {
+  return new Promise(function(resolve, reject) {
+    ECS.getConfiguration(fakeToken).then(reject, function(error) {
+      try {
+        assert(error instanceof klass);
+        assert.equal(error.code, code);
+        if (message) {
+          assert.equal(error.message, message);
+        }
+        resolve();
+      } catch (e) {
+        reject(e);
+      }
+    });
+  });
+}
