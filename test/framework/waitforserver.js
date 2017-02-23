1be6b95a9e2a8232ac51b377b6836c46b4cc8b49
Mark Andrus Roberts <mroberts@twilio.com>
2017-02-23 14:56:39 -0800

    wait after OPTIONS check; add per-Framework timeouts

--- test/framework/waitforserver.js
+++ test/framework/waitforserver.js
@@ -3,16 +3,28 @@
 const http = require('http');
 
 /**
+ * This function returns a Promise that resolves after a specified delay.
+ * @param {number} [delay=200] - delay in ms
+ * @returns {Promise<undefined>}
+ */
+function wait(delay) {
+  delay = delay || 200;
+  return new Promise(resolve => setTimeout(resolve, delay));
+}
+
+/**
  * This function returns a Promise that resolves once the specified HTTP server
  * responds to an OPTIONS request. The OPTIONS request will be retried until
  * a configurable timeout occurs, at which point the Promise rejects.
  * @param {string} host - the HTTP server host
  * @param {number} port - the HTTP server port
  * @param {number} [timeout=30000] - timeout in ms
+ * @param {number} [delay=200] - delay in ms
  * @returns {Promise<undefined>}
  */
-function waitForServer(host, port, timeout) {
+function waitForServer(host, port, timeout, delay) {
   timeout = timeout || 30000;
+  delay = delay || 200;
 
   return new Promise((resolve, reject) => {
     let timedout = false;
@@ -29,7 +41,7 @@ function waitForServer(host, port, timeout) {
         resolve();
       }, () => {
         if (!timedout) {
-          return sendOptionsRequest();
+          return wait(delay).then(sendOptionsRequest);
         }
       });
     }
