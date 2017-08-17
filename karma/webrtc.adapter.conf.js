857f44c414b67c8101886c2c81a1e6df968b5022
Manjesh Malavalli <mmalavalli@twilio.com>
2017-08-17 12:42:11 -0700

    JSDK-1449 * Removing webrtc shims from the project. * Adding '@twilio/webrtc' as a dependency.

--- karma/webrtc.adapter.conf.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-const makeConf = require('./makeconf');
-const { join } = require('path');
-
-module.exports = makeConf(join('..', 'test', 'webrtc.js'), null, [
-  require.resolve('webrtc-adapter/out/adapter')
-]);
