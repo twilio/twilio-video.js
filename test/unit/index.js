37299913b03875c600504a2c6e31d0fbb2725024
Manjesh Malavalli <mmalavalli@twilio.com>
2017-11-26 19:42:33 -0800

    Creating the Identity and MID TrackMatcher modules. Moving the OrderedTrackMatcher module from twilio-webrtc.

--- test/unit/index.js
+++ test/unit/index.js
@@ -49,6 +49,8 @@ require('./spec/util');
 require('./spec/util/insightspublisher');
 require('./spec/util/log');
 require('./spec/util/sdp');
+require('./spec/util/trackmatcher/mid');
+require('./spec/util/trackmatcher/ordered');
 require('./spec/util/twilioerror');
 
 require('./spec/webaudio/audiocontext');
