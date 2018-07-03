3740cca38b59735ee7e1a80e15ce063a3c3e86a2
twilio-ci <twilio-ci@twilio.com>
2018-07-03 22:42:25 +0000

    1.11.0

---
+++ es5/data/transport.js
@@ -0,0 +1,84 @@
+'use strict';
+
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
+var _require = require('events'),
+    EventEmitter = _require.EventEmitter;
+
+/**
+ * @classdesc A {@link DataTransport} implements {@link MediaSignalingTransport}
+ *   in terms of an RTCDataChannel.
+ * @extends EventEmitter
+ * @implements MediaSignalingTransport
+ * @emits DataTransport#message
+ */
+
+
+var DataTransport = function (_EventEmitter) {
+  _inherits(DataTransport, _EventEmitter);
+
+  /**
+   * Construct a {@link DataTransport}.
+   * @param {RTCDataChannel} dataChannel
+   */
+  function DataTransport(dataChannel) {
+    _classCallCheck(this, DataTransport);
+
+    var _this = _possibleConstructorReturn(this, (DataTransport.__proto__ || Object.getPrototypeOf(DataTransport)).call(this));
+
+    Object.defineProperties(_this, {
+      _dataChannel: {
+        value: dataChannel
+      }
+    });
+
+    dataChannel.addEventListener('message', function (_ref) {
+      var data = _ref.data;
+
+      try {
+        var message = JSON.parse(data);
+        _this.emit('message', message);
+      } catch (error) {
+        // Do nothing.
+      }
+    });
+    return _this;
+  }
+
+  /**
+   * Publish a message. Returns true if calling the method resulted in
+   * publishing (or eventually publishing) the update.
+   * @param {object} message
+   * @returns {boolean}
+   */
+
+
+  _createClass(DataTransport, [{
+    key: 'publish',
+    value: function publish(message) {
+      try {
+        var data = JSON.stringify(message);
+        this._dataChannel.send(data);
+      } catch (error) {
+        return false;
+      }
+      return true;
+    }
+  }]);
+
+  return DataTransport;
+}(EventEmitter);
+
+/**
+ * The {@link DataTransport} received a message.
+ * @event DataTransport#message
+ * @param {object} message
+ */
+
+module.exports = DataTransport;
