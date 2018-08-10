df895904eb452422b975a6f47d378d27c3593e8c
twilio-ci <twilio-ci@twilio.com>
2018-08-10 18:22:13 +0000

    2.0.0-beta1

---
+++ es5/media/track/es5/localdatatrack.js
@@ -0,0 +1,18 @@
+// TODO(mroberts): Remove this when we go to the next major version. This is
+// only in place so that we can support ES6 classes without requiring `new`.
+'use strict';
+
+var _require = require('util'),
+    inherits = _require.inherits;
+
+var LocalDataTrackClass = require('../localdatatrack');
+
+function LocalDataTrack(options) {
+  var track = new LocalDataTrackClass(options);
+  Object.setPrototypeOf(track, LocalDataTrack.prototype);
+  return track;
+}
+
+inherits(LocalDataTrack, LocalDataTrackClass);
+
+module.exports = LocalDataTrack;
