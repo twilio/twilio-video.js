c9baf54a14fd509c9d38230b1c81337550a0bef9
twilio-ci <twilio-ci@twilio.com>
2018-12-02 20:26:45 +0000

    1.14.2-rc1

---
+++ es5/util/support.js
@@ -0,0 +1,30 @@
+/* globals RTCPeerConnection, webkitRTCPeerConnection, mozRTCPeerConnection, navigator */
+'use strict';
+
+var guessBrowser = require('./').guessBrowser;
+
+/**
+ * Check whether PeerConnection API is supported.
+ * @returns {boolean}
+ */
+function isRTCPeerConnectionSupported() {
+  return typeof RTCPeerConnection !== 'undefined' || typeof webkitRTCPeerConnection !== 'undefined' || typeof mozRTCPeerConnection !== 'undefined';
+}
+
+/**
+ * Check whether GetUserMedia API is supported.
+ * @returns {boolean}
+ */
+function isGetUserMediaSupported() {
+  return !!(navigator.mediaDevices && navigator.mediaDevices.getUserMedia) || !!navigator.getUserMedia || !!navigator.webkitGetUserMedia || !!navigator.mozGetUserMedia;
+}
+
+/**
+ * Check if the current environment is supported by the SDK.
+ * @returns {boolean}
+ */
+function isSupported() {
+  return !!guessBrowser() && isGetUserMediaSupported() && isRTCPeerConnectionSupported();
+}
+
+module.exports = isSupported;
