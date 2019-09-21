91157b2e382ae844077df7d52ab46e8c1320149a
Makarand Patwardhan <mpatwardhan@twilio.com>
2019-09-20 18:14:07 -0700

    WIP

--- scripts/circleci-run-tests.sh
+++ scripts/circleci-run-tests.sh
@@ -1,6 +1,21 @@
 #!/bin/bash
 set -ev
 echo PWD=$PWD
-docker-compose build test
-docker-compose run test npm run test:integration
+
+echo "node version:"
+node --version
+echo "npm version:"
+npm --version
+echo "os info:"
+uname -a
+echo "directory:"
+ls -alt
+echo "printenv"
+printenv
+# docker-compose build test
+# docker-compose run test npm run test:integration
+
+mkdir logs
+echo 'test results go here!' >> logs/results.txt
+
 
