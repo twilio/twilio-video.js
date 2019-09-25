f147dee82f3b86b57e4804705f0cc518bab13cb3
Makarand Patwardhan <mpatwardhan@twilio.com>
2019-09-25 06:10:01 -0700

    Edge is not supported

---
+++ test/unit/spec/util/support.js
@@ -0,0 +1,85 @@
+'use strict';
+
+const assert = require('assert');
+// const { EventEmitter } = require('events');
+// const sinon = require('sinon');
+
+const isSupported = require('../../../../lib/util/support');
+
+describe.only('isSupported', () => {
+
+  let oldAgent;
+  before(() => {
+    // save useragent.
+    oldAgent = navigator.userAgent;
+  });
+
+  after(() => {
+    // restore useragent.
+    navigator.userAgent = oldAgent;
+  });
+
+  [
+    [
+      'chrome on windows',
+      'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.90 Safari/537.36'
+    ],
+    [
+      'chrome on mac',
+      'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.90 Safari/537.36'
+    ],
+    [
+      'safari on mac',
+      'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13 Safari/605.1.15'
+    ],
+    [
+      'safari on ipad',
+      'Mozilla/5.0 (iPad; CPU OS 12_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13 Mobile/15E148 Safari/604.1'
+    ],
+    [
+      'safari on iphone touch',
+      'Mozilla/5.0 (iPhone; CPU iPhone OS 12_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13 Mobile/15E148 Safari/604.1'
+    ],
+    [
+      'firefox on windows',
+      'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:54.0) Gecko/20100101 Firefox/69.0'
+    ],
+    [
+      'firefox on mac',
+      'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.13; rv:61.0) Gecko/20100101 Firefox/69.0'
+    ],
+    [
+      'firefox on linux',
+      'Mozilla/5.0 (X11; Linux i586; rv:31.0) Gecko/20100101 Firefox/69.0'
+    ],
+  ].forEach(([browser, useragent]) => {
+    it('returns true for: ' + browser, () => {
+      navigator.userAgent = useragent;
+      assert.equal(isSupported(), true);
+    });
+  });
+
+  [
+    [
+      'Edge 42',
+      'Mozilla/5.0 (Windows NT 10.0; Win64; x64; Xbox; Xbox One) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.116 Safari/537.36 Edge/15.15063'
+    ],
+    [
+      'Edge 25',
+      'Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/46.0.2486.0 Safari/537.36 Edge/13.10586'
+    ],
+    [
+      'Another Edge',
+      'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.71 Safari/537.36 Edge/12.0'
+    ],
+    [
+      'Just anything with Edge in it',
+      'Foo Edge Bar'
+    ],
+  ].forEach(([browser, useragent]) => {
+    it('returns false for unsupported browser : ' + browser, () => {
+      navigator.userAgent = useragent;
+      assert.equal(isSupported(), false);
+    });
+  });
+});
