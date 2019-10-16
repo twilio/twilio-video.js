b9f8bb15338ba204a101476f54f15e16a2543621
Makarand Patwardhan <mpatwardhan@twilio.com>
2019-10-15 20:49:22 -0700

    incorporated Manjesh's PR feedabck

--- test/integration/spec/localtrackpublication.js
+++ test/integration/spec/localtrackpublication.js
@@ -9,6 +9,7 @@ const {
   LocalDataTrack
 } = require('../../../lib');
 
+const { trackPriority: { PRIORITY_HIGH, PRIORITY_LOW, PRIORITY_STANDARD } } = require('../../../lib/util/constants');
 const RemoteAudioTrack = require('../../../lib/media/track/remoteaudiotrack');
 const RemoteAudioTrackPublication = require('../../../lib/media/track/remoteaudiotrackpublication');
 const RemoteDataTrack = require('../../../lib/media/track/remotedatatrack');
@@ -24,10 +25,17 @@ const { isFirefox } = require('../../lib/guessbrowser');
 const {
   capitalize,
   combinationContext,
+  createSyntheticAudioStreamTrack,
   participantsConnected,
   randomName,
+  setup,
+  smallVideoConstraints,
+  trackPublishPriorityChanged,
   tracksSubscribed,
   tracksUnpublished,
+  trackSwitchedOff,
+  trackSwitchedOn,
+  waitFor,
   waitForTracks
 } = require('../../lib/util');
 
@@ -211,5 +219,111 @@ describe('LocalTrackPublication', function() {
         }
       });
     });
+  });
+
+  describe('#setPriority', () => {
+    let thisRoom;
+    let thoseRooms;
+    let aliceLocal;
+    let bobLocal;
+    let aliceRemote;
+    let bobRemote;
+    let aliceTracks;
+    let bobTracks;
+    let aliceRemoteVideoTrack;
+    let bobRemoteVideoTrack;
+    let aliceLocalVideoTrackPublication;
+    let bobLocalVideoTrackPublication;
+    let aliceRemoteVideoTrackPublication;
+    let bobRemoteVideoTrackPublication;
+
+    beforeEach(async () => {
+      const dataTrack = new LocalDataTrack();
+      [, thisRoom, thoseRooms] = await setup({
+        testOptions: {
+          bandwidthProfile: {
+            video: { maxTracks: 1, dominantSpeakerPriority: 'low' }
+          },
+          tracks: [dataTrack]
+        },
+        otherOptions: { tracks: [dataTrack] },
+        nTracks: 0
+      });
+
+      [aliceTracks, bobTracks] = await Promise.all([1, 2, 3].map(async () => [
+        createSyntheticAudioStreamTrack() || await createLocalAudioTrack({ fake: true }),
+        await createLocalVideoTrack(smallVideoConstraints),
+      ]));
+
+      [aliceLocal, bobLocal] = thoseRooms.map(room => room.localParticipant);
+      [aliceRemote, bobRemote] = [thisRoom.participants.get(aliceLocal.sid), thisRoom.participants.get(bobLocal.sid)];
+
+      // Alice publishes her tracks at low priority
+      // Bob publishes his tracks at standard priority
+      await Promise.all([
+        ...aliceTracks.map(track => aliceLocal.publishTrack(track, { priority: PRIORITY_LOW })),
+        ...bobTracks.map(track => bobLocal.publishTrack(track, { priority: PRIORITY_STANDARD })),
+        tracksSubscribed(aliceRemote, 3),
+        tracksSubscribed(bobRemote, 3)
+      ]);
+
+      [aliceRemoteVideoTrack, bobRemoteVideoTrack] = [aliceRemote, bobRemote].map(({ videoTracks }) => {
+        return [...videoTracks.values()][0].track;
+      });
+
+      [aliceLocalVideoTrackPublication, bobLocalVideoTrackPublication] = [aliceLocal, bobLocal].map(({ videoTracks }) => {
+        return [...videoTracks.values()][0];
+      });
+
+      [aliceRemoteVideoTrackPublication, bobRemoteVideoTrackPublication] = [aliceRemote, bobRemote].map(({ videoTracks }) => {
+        return [...videoTracks.values()][0];
+      });
+
+    });
+
+    afterEach(async () => {
+      [thisRoom, ...thoseRooms].forEach(room => room && room.disconnect());
+      [...aliceTracks, ...bobTracks].forEach(track => track.stop && track.stop());
+      if (thisRoom) {
+        await completeRoom(thisRoom.sid);
+      }
+    });
+
+    if (defaults.topology !== 'peer-to-peer') {
+      it('publisher can upgrade track\'s priority', async () => {
+        await waitFor([
+          trackSwitchedOn(bobRemoteVideoTrack),
+          trackSwitchedOff(aliceRemoteVideoTrack)
+        ], 'Bobs track to get switched On, and Alice Switched Off');
+
+        // Alice changes her track priority to high
+        aliceLocalVideoTrackPublication.setPriority(PRIORITY_HIGH);
+
+        // expect Alice's track to get switched on, and Bob's track to get switched off
+        await waitFor([
+          trackSwitchedOn(aliceRemoteVideoTrack),
+          trackSwitchedOff(bobRemoteVideoTrack),
+          trackPublishPriorityChanged(aliceRemoteVideoTrackPublication)
+        ], 'Alice track to get switched On, and Bob Switched Off');
+      });
+
+      it('publisher can downgrade track\'s priority', async () => {
+        await waitFor([
+          trackSwitchedOn(bobRemoteVideoTrack),
+          trackSwitchedOff(aliceRemoteVideoTrack)
+        ], 'Bobs track to get switched On, and Alice Switched Off');
+
+        // Bob changes his track priority to low
+        bobLocalVideoTrackPublication.setPriority(PRIORITY_LOW);
+
+        // expect Alice's track to get switched on, and Bob's track to get switched off
+        // also expect to receive publishPriorityChanged event on Bob's track.
+        await waitFor([
+          trackSwitchedOn(aliceRemoteVideoTrack),
+          trackSwitchedOff(bobRemoteVideoTrack),
+          trackPublishPriorityChanged(bobRemoteVideoTrackPublication)
+        ], 'Alice track to get switched On, and Bob Switched Off');
+      });
+    }
   });
 });
