4c54d812465fb921683b913d19d61234f2b07d13
Makarand Patwardhan <mpatwardhan@twilio.com>
2019-10-17 14:07:22 -0700

    limit regions

--- scripts/circleci-run-tests.sh
+++ scripts/circleci-run-tests.sh
@@ -19,12 +19,18 @@ dev)
   export ACCOUNT_SID=${ACCOUNT_SID_DEV}
   export API_KEY_SECRET=${API_KEY_SECRET_DEV}
   export API_KEY_SID=${API_KEY_SID_DEV}
+  export REGIONS='us1'
+  export ECS_SERVER=https://ecs.dev-us1.twilio.com
+  export WS_SERVER_INSIGHTS=wss://sdkgw.dev-us1.twilio.com/v1/VideoEvents
   ;;
 stage)
   echo "Testing against stage"
   export ACCOUNT_SID=${ACCOUNT_SID_STAGE}
   export API_KEY_SECRET=${API_KEY_SECRET_STAGE}
   export API_KEY_SID=${API_KEY_SID_STAGE}
+  export REGIONS='au1,ie1,us1'
+  export ECS_SERVER=https://ecs.stage-us1.twilio.com
+  export WS_SERVER_INSIGHTS=wss://sdkgw.stage-us1.twilio.com/v1/VideoEvents
   ;;
 prod)
   echo "Testing against prod"
