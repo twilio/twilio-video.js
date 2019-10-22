bcd4b8d8fc54f0eccfe20b5a6f91a41e0fbd8193
Makarand Patwardhan <mpatwardhan@twilio.com>
2019-10-21 19:15:22 -0700

    integration test

--- test/integration/spec/bandwidthprofile.js
+++ test/integration/spec/bandwidthprofile.js
@@ -20,6 +20,7 @@ const {
 } = require('../../lib/util');
 
 const { trackPriority: { PRIORITY_HIGH, PRIORITY_LOW, PRIORITY_STANDARD } } = require('../../../lib/util/constants');
+const { trackSwitchOfMode: { MODE_DISABLED, MODE_PREDICTED } } = require('../../../lib/util/constants');
 
 describe('Bandwidth Management', function() {
   // eslint-disable-next-line no-invalid-this
@@ -137,6 +138,71 @@ describe('Bandwidth Management', function() {
         });
 
         afterEach(async () => {
+          [thisRoom, ...thoseRooms].forEach(room => room && room.disconnect());
+          if (thisRoom) {
+            await completeRoom(thisRoom.sid);
+          }
+        });
+      });
+    });
+
+    describe.only('bandwidthProfile.video.trackSwitchOffMode', () => {
+      [MODE_DISABLED, MODE_PREDICTED].forEach((trackSwitchOffMode) => {
+        const expectSwitchOff = (trackSwitchOffMode !== MODE_DISABLED);
+        it(`${expectSwitchOff ? 'Should' : 'Should not'} switch off tracks when trackSwitchOffMode = ${trackSwitchOffMode}`, async () => {
+          const [, thisRoom, thoseRooms] = await setup({
+            testOptions: {
+              bandwidthProfile: {
+                video: {
+                  trackSwitchOffMode,
+                  dominantSpeakerPriority: PRIORITY_LOW,
+                  maxTracks: 1
+                }
+              },
+              dominantSpeaker: true,
+              tracks: []
+            },
+            otherOptions: { tracks: [] },
+            nTracks: 0
+          });
+
+          const [aliceTracks, bobTracks] = await waitFor([1, 2].map(async () => [
+            createSyntheticAudioStreamTrack() || await createLocalAudioTrack({ fake: true }),
+            await createLocalVideoTrack(smallVideoConstraints)
+          ]), 'local tracks');
+
+          // Initially disable Alice's audio
+          aliceTracks[0].enabled = false;
+
+          const [aliceLocal, bobLocal] = thoseRooms.map(room => room.localParticipant);
+          const [aliceRemote, bobRemote] = [thisRoom.participants.get(aliceLocal.sid), thisRoom.participants.get(bobLocal.sid)];
+
+          // Alice publishes her track at low Pri. Bob publish his at Hi Pri
+          await waitFor([
+            ...aliceTracks.map(track => aliceLocal.publishTrack(track, { priority: PRIORITY_LOW })),
+            ...bobTracks.map(track => bobLocal.publishTrack(track, { priority: PRIORITY_HIGH })),
+            tracksSubscribed(aliceRemote, 2),
+            tracksSubscribed(bobRemote, 2)
+          ], 'all tracks to get published and subscribed');
+
+          const [aliceRemoteVideoTrack, bobRemoteVideoTrack] = [aliceRemote, bobRemote].map(({ videoTracks }) => {
+            return [...videoTracks.values()][0].track;
+          });
+
+          // Bob should be the Dominant Speaker
+          await waitFor([
+            dominantSpeakerChanged(thisRoom, bobRemote),
+            trackSwitchedOn(bobRemoteVideoTrack),
+            expectSwitchOff ? trackSwitchedOff(aliceRemoteVideoTrack) : trackSwitchedOn(aliceRemoteVideoTrack)
+          ], 'Bob to be dominant speaker');
+
+          bobRemote.videoTracks.forEach(({ track }) => {
+            assert.equal(track.isSwitchedOff, !expectSwitchOff);
+          });
+
+          aliceRemote.videoTracks.forEach(({ track }) => {
+            assert.equal(track.isSwitchedOff, true);
+          });
           [thisRoom, ...thoseRooms].forEach(room => room && room.disconnect());
           if (thisRoom) {
             await completeRoom(thisRoom.sid);
