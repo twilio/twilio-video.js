9c721fd6f933e633edbb7d7ebb0790bc5ce488b5
twilio-ci <twilio-ci@twilio.com>
2019-10-31 01:18:42 +0000

    2.0.0-rc47

---
+++ es5/media/track/remotevideotrack.js
@@ -0,0 +1,102 @@
+'use strict';
+
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
+var mixinRemoteMediaTrack = require('./remotemediatrack');
+var VideoTrack = require('./videotrack');
+
+var RemoteMediaVideoTrack = mixinRemoteMediaTrack(VideoTrack);
+
+/**
+ * A {@link RemoteVideoTrack} represents a {@link VideoTrack} published to a
+ * {@link Room} by a {@link RemoteParticipant}.
+ * @extends VideoTrack
+ * @property {boolean} isEnabled - Whether the {@link RemoteVideoTrack} is enabled
+ * @property {boolean} isSwitchedOff - Whether the {@link RemoteVideoTrack} is switched off
+ * @property {Track.SID} sid - The {@link RemoteVideoTrack}'s SID
+ * @property {?Track.Priority} priority - The subscribe priority of the {@link RemoteVideoTrack}
+ * @emits RemoteVideoTrack#dimensionsChanged
+ * @emits RemoteVideoTrack#disabled
+ * @emits RemoteVideoTrack#enabled
+ * @emits RemoteVideoTrack#started
+ * @emits RemoteVideoTrack#switchedOff
+ * @emits RemoteVideoTrack#switchedOn
+ */
+
+var RemoteVideoTrack = function (_RemoteMediaVideoTrac) {
+  _inherits(RemoteVideoTrack, _RemoteMediaVideoTrac);
+
+  /**
+   * Construct a {@link RemoteVideoTrack}.
+   * @param {Track.SID} sid - The {@link RemoteVideoTrack}'s SID
+   * @param {MediaTrackReceiver} mediaTrackReceiver - A video MediaStreamTrack container
+   * @param {boolean} isEnabled - whether the {@link RemoteVideoTrack} is enabled
+   * @param {function(?Track.Priority): void} setPriority - Set or clear the subscribe
+   *  {@link Track.Priority} of the {@link RemoteVideoTrack}
+   * @param {{log: Log}} options - The {@link RemoteTrack} options
+   */
+  function RemoteVideoTrack(sid, mediaTrackReceiver, isEnabled, setPriority, options) {
+    _classCallCheck(this, RemoteVideoTrack);
+
+    return _possibleConstructorReturn(this, (RemoteVideoTrack.__proto__ || Object.getPrototypeOf(RemoteVideoTrack)).call(this, sid, mediaTrackReceiver, isEnabled, setPriority, options));
+  }
+
+  _createClass(RemoteVideoTrack, [{
+    key: 'toString',
+    value: function toString() {
+      return '[RemoteVideoTrack #' + this._instanceId + ': ' + this.sid + ']';
+    }
+  }]);
+
+  return RemoteVideoTrack;
+}(RemoteMediaVideoTrack);
+
+/**
+ * The {@link RemoteVideoTrack}'s dimensions changed.
+ * @param {RemoteVideoTrack} track - The {@link RemoteVideoTrack} whose
+ *   dimensions changed
+ * @event RemoteVideoTrack#dimensionsChanged
+ */
+
+/**
+ * The {@link RemoteVideoTrack} was disabled, i.e. "paused".
+ * @param {RemoteVideoTrack} track - The {@link RemoteVideoTrack} that was
+ *   disabled
+ * @event RemoteVideoTrack#disabled
+ */
+
+/**
+ * The {@link RemoteVideoTrack} was enabled, i.e. "unpaused".
+ * @param {RemoteVideoTrack} track - The {@link RemoteVideoTrack} that was
+ *   enabled
+ * @event RemoteVideoTrack#enabled
+ */
+
+/**
+ * The {@link RemoteVideoTrack} started. This means there is enough video data
+ * to begin playback.
+ * @param {RemoteVideoTrack} track - The {@link RemoteVideoTrack} that started
+ * @event RemoteVideoTrack#started
+ */
+
+/**
+ * A {@link RemoteVideoTrack} was switched off.
+ * @param {RemoteVideoTrack} track - The {@link RemoteVideoTrack} that was
+ *   switched off
+ * @event RemoteVideoTrack#switchedOff
+ */
+
+/**
+ * A {@link RemoteVideoTrack} was switched on.
+ * @param {RemoteVideoTrack} track - The {@link RemoteVideoTrack} that was
+ *   switched on
+ * @event RemoteVideoTrack#switchedOn
+ */
+
+module.exports = RemoteVideoTrack;
---
+++ es5/util/support.js
@@ -0,0 +1,45 @@
+/* globals RTCPeerConnection, webkitRTCPeerConnection, mozRTCPeerConnection, navigator */
+'use strict';
+
+var _require = require('@twilio/webrtc/lib/util'),
+    guessBrowser = _require.guessBrowser;
+
+/**
+ * Check whether PeerConnection API is supported.
+ * @returns {boolean}
+ */
+
+
+function isRTCPeerConnectionSupported() {
+  return typeof RTCPeerConnection !== 'undefined' || typeof webkitRTCPeerConnection !== 'undefined' || typeof mozRTCPeerConnection !== 'undefined';
+}
+
+/**
+ * Check whether GetUserMedia API is supported.
+ * @returns {boolean}
+ */
+function isGetUserMediaSupported() {
+  return !!(navigator.mediaDevices && navigator.mediaDevices.getUserMedia) || !!navigator.getUserMedia || !!navigator.webkitGetUserMedia || !!navigator.mozGetUserMedia;
+}
+
+/**
+ * @returns {boolean} - true if the browser is explicitly unsupported.
+ *  Note: this function returning false does not mean that browser is supported.
+ */
+function isExplicitlyUnsupportedBrowser() {
+  if (typeof navigator !== 'undefined' && typeof navigator.userAgent === 'string') {
+    return (/Edg(e)?/.test(navigator.userAgent)
+    );
+  }
+  return false;
+}
+
+/**
+ * Check if the current environment is supported by the SDK.
+ * @returns {boolean}
+ */
+function isSupported() {
+  return !isExplicitlyUnsupportedBrowser() && !!guessBrowser() && isGetUserMediaSupported() && isRTCPeerConnectionSupported();
+}
+
+module.exports = isSupported;
