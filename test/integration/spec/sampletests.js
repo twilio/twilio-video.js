dc7a319bf60fb694373372b93e8fa17984c33148
Makarand Patwardhan <mpatwardhan@twilio.com>
2019-11-20 15:54:54 -0800

    mocha grep works for context as well

--- test/integration/spec/sampletests.js
+++ test/integration/spec/sampletests.js
@@ -4,6 +4,16 @@ const assert = require('assert');
 // const defaults = require('../../lib/defaults');
 
 describe('test suites', () => {
+  context('@unstable foo', () => {
+    it('a sample test', () => {
+      assert(true);
+    });
+  });
+  context('@stable bar', () => {
+    it('a sample test', () => {
+      assert(true);
+    });
+  });
   it('a sample @stable test', () => {
     assert(true);
   });
