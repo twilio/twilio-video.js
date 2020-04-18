7384c1c9f875af95c260e7edf3a7626c3dce455a
joma <joma@twilio.com>
2020-04-17 17:10:40 -0700

    Wrapped constructor tests in describe block

--- test/unit/spec/util/networkmonitor.js
+++ test/unit/spec/util/networkmonitor.js
@@ -5,31 +5,33 @@ const sinon = require('sinon');
 
 const NetworkMonitor = require('../../../../lib/util/networkmonitor');
 
-describe.only('networkMonitor', () => {
+describe.only('NetworkMonitor', () => {
   const onNetworkChangedCalled = sinon.spy();
 
-  it('should return an instance of NetworkMonitor', () => {
-    const networkMonitor = new NetworkMonitor();
-    assert(networkMonitor instanceof NetworkMonitor);
-  });
-
-  [true, false].forEach(bool => {
-    it(`should return ${bool} when accessing isOnline`, () => {
-      navigator.onLine = bool;
-      const networkMonitor = new NetworkMonitor(onNetworkChangedCalled, { navigator });
-      assert.equal(networkMonitor.isOnline, bool);
+  describe('constructor', () => {
+    it('should return an instance of NetworkMonitor', () => {
+      const networkMonitor = new NetworkMonitor();
+      assert(networkMonitor instanceof NetworkMonitor);
     });
-  });
 
-  [{ connection: { type: 'wifi' } }, { connection: '' }, {}].forEach(el => {
-    it(`should return ${(el.connection && el.connection.type) || null}`, () => {
-      const networkMonitor = new NetworkMonitor(onNetworkChangedCalled, { navigator: el });
+    [true, false].forEach(bool => {
+      it(`should return ${bool} when accessing isOnline`, () => {
+        const navigator = { onLine: bool };
+        const networkMonitor = new NetworkMonitor(onNetworkChangedCalled, { navigator });
+        assert.equal(networkMonitor.isOnline, bool);
+      });
+    });
 
-      if (el.connection && el.connection.type) {
-        assert.equal(networkMonitor.type, el.connection.type);
-      } else {
-        assert.equal(networkMonitor.type, null);
-      }
+    [{ connection: { type: 'wifi' } }, { connection: {} }, {}].forEach(el => {
+      it(`should return ${(el.connection && el.connection.type) || null}`, () => {
+        const networkMonitor = new NetworkMonitor(onNetworkChangedCalled, { navigator: el });
+
+        if (el.connection && el.connection.type) {
+          assert.equal(networkMonitor.type, el.connection.type);
+        } else {
+          assert.equal(networkMonitor.type, null);
+        }
+      });
     });
   });
 });
