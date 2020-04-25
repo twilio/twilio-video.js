bc5c3db39f34b4c57078548d3e11be63c37cb4a3
Manjesh Malavalli <manjesh.github@gmail.com>
2020-04-24 23:02:31 -0700

    Refactoring NetworkMonitor implementation and unit tests.

--- test/unit/spec/util/networkmonitor.js
+++ test/unit/spec/util/networkmonitor.js
@@ -5,155 +5,112 @@ const sinon = require('sinon');
 
 const MockConnection = require('../../../lib/mockconnection');
 const EventTarget = require('../../../../lib/eventtarget');
+const { combinationContext } = require('../../../lib/util');
+
 const NetworkMonitor = require('../../../../lib/util/networkmonitor');
 
 describe('NetworkMonitor', () => {
-  let onNetworkChangedCalled;
-  let nav;
-  let win;
-
-  beforeEach(() => {
-    onNetworkChangedCalled = sinon.spy();
-    nav = {
-      onLine: true,
-      connection: new MockConnection(),
-    };
-
-    win = new EventTarget();
-  });
-
   describe('constructor', () => {
     it('should return an instance of NetworkMonitor', () => {
-      const networkMonitor = new NetworkMonitor();
+      const networkMonitor = new NetworkMonitor(() => {
+      });
       assert(networkMonitor instanceof NetworkMonitor);
     });
 
-    [true, false].forEach(bool => {
-      it(`should return ${bool} when accessing isOnline`, () => {
-        nav.onLine = bool;
-        const networkMonitor = new NetworkMonitor(onNetworkChangedCalled, { navigator: nav });
-        assert.equal(networkMonitor.isOnline, bool);
-      });
-    });
-
-    [{ connection: { type: 'wifi' } }, { connection: {} }, {}].forEach(el => {
-      it(`should return ${(el.connection && el.connection.type) || null} when accessing type`, () => {
-        const networkMonitor = new NetworkMonitor(onNetworkChangedCalled, { navigator: el });
-
-        if (el.connection && el.connection.type) {
-          assert.equal(networkMonitor.type, el.connection.type);
-        } else {
-          assert.equal(networkMonitor.type, null);
-        }
-      });
-    });
-  });
-
-  describe('start and stop methods', () => {
-    let networkMonitor;
-
-    describe('window', () => {
-      beforeEach(() => {
-        networkMonitor = new NetworkMonitor(onNetworkChangedCalled, { window: win });
-      });
-
-      describe('#start', () => {
-        it('should call start once for online event on target window', () => {
-          win.addEventListener = sinon.spy();
-          networkMonitor.start();
-          sinon.assert.calledOnce(win.addEventListener);
-        });
-      });
-
-      describe('#stop', () => {
-        it('should call stop once for online event on target window', () => {
-          win.removeEventListener = sinon.spy();
-          networkMonitor.stop();
-          sinon.assert.calledOnce(win.removeEventListener);
+    [undefined, false, true].forEach(onLine => {
+      const expectedIsOnline = typeof onLine === 'boolean' ? onLine : true;
+      const navigator = typeof onLine === 'boolean' ? { onLine } : {};
+      context(`when navigator.onLine is ${typeof onLine === 'boolean' ? onLine : 'not present'}`, () => {
+        it(`should set .isOnline to ${expectedIsOnline}`, () => {
+          const networkMonitor = new NetworkMonitor(() => {
+          }, { navigator });
+          assert.equal(networkMonitor.isOnline, expectedIsOnline);
         });
       });
     });
 
-    describe('navigator', () => {
-      beforeEach(() => {
-        networkMonitor = new NetworkMonitor(onNetworkChangedCalled, { navigator: nav });
-      });
-
-      describe('#start', () => {
-        it('should call start twice for each event on target nav', () => {
-          nav.connection.addEventListener = sinon.spy();
-          networkMonitor.start();
-          sinon.assert.calledTwice(nav.connection.addEventListener);
-        });
-      });
-
-      describe('#stop', () => {
-        it('should call stop twice for each event on target nav', () => {
-          nav.connection.removeEventListener = sinon.spy();
-          networkMonitor.stop();
-          sinon.assert.calledTwice(nav.connection.removeEventListener);
+    [undefined, new MockConnection(), new MockConnection('whiz')].forEach(connection => {
+      const expectedType = connection && connection.type ? connection.type : null;
+      const navigator = connection ? { connection } : {};
+      context(`when navigator.connection${connection ? `.type ${connection.type ? 'exists' : 'does not exist'}` : ' does not exist'}`, () => {
+        it(`should set .type to ${expectedType}`, () => {
+          const networkMonitor = new NetworkMonitor(() => {
+          }, { navigator });
+          assert.equal(networkMonitor.type, expectedType);
         });
       });
     });
   });
+
+  describe('#start', () => testStartOrStop('start'));
+
+  describe('#stop', () => testStartOrStop('stop'));
 
   describe('onNetworkChanged', () => {
-    let networkMonitor;
-    let networkChangedCalled;
-
-    describe('when navigator.connection.type is a string', () => {
-      beforeEach(() => {
-        networkChangedCalled = sinon.spy();
-        networkMonitor = new NetworkMonitor(networkChangedCalled, { navigator: nav, window: win });
-      });
-
-      afterEach(() => {
-        delete nav.connection.type;
-      });
-
-      it('should call the function when network has typechanged on navigator', () => {
+    combinationContext([
+      [
+        [undefined, new MockConnection(), new MockConnection('whiz'), new MockConnection('change'), new MockConnection('typechange')],
+        x => `when navigator.connection${x ? `.type ${{
+          whiz: 'does not change',
+          change: 'changes with "change" event',
+          typechange: 'changes with "typechange" event'
+        }[x.type] || 'does not exist'}` : ' does not exist'}`
+      ],
+      [
+        [true, false],
+        onLine => `when navigator.onLine changes from ${onLine} to ${!onLine}`
+      ]
+    ], ([connection, onLine]) => {
+      const shouldCallNetworkChanged = !(onLine || (connection && connection.type && connection.type === 'whiz'));
+      it(`should ${shouldCallNetworkChanged ? '' : 'not '}call onNetworkChanged`, () => {
+        const navigator = {
+          connection,
+          onLine
+        };
+        const onNetworkChanged = sinon.spy();
+        const window = new EventTarget();
+        const networkMonitor = new NetworkMonitor(onNetworkChanged, {
+          navigator,
+          window
+        });
         networkMonitor.start();
-        nav.connection.type = 'bang';
-        nav.connection.dispatchEvent({ type: 'typechange' });
-        sinon.assert.calledOnce(networkChangedCalled);
-      });
-
-      it('should call the function when network has changed on navigator', () => {
-        networkMonitor.start();
-        nav.connection.type = 'foobar';
-        nav.connection.dispatchEvent({ type: 'change' });
-        sinon.assert.calledOnce(networkChangedCalled);
-      });
-    });
-
-    describe('when navigator.connection.type is not available (rely on window.ononline)', () => {
-      const navNoConnectionType = {
-        connection: {}
-      };
-
-      beforeEach(() => {
-        networkChangedCalled = sinon.spy();
-        networkMonitor = new NetworkMonitor(networkChangedCalled, { navigator: navNoConnectionType, window: win });
-      });
-
-      it('should call the function on window', () => {
-        networkMonitor.start();
-        win.dispatchEvent({ type: 'online' });
-        sinon.assert.calledOnce(networkChangedCalled);
-      });
-    });
-
-    describe('when navigator.connection is not available (rely on window.ononline)', () => {
-      beforeEach(() => {
-        networkChangedCalled = sinon.spy();
-        networkMonitor = new NetworkMonitor(networkChangedCalled, { navigator: {}, window: win });
-      });
-
-      it('should call the function when event is online on window', () => {
-        networkMonitor.start();
-        win.dispatchEvent({ type: 'online' });
-        sinon.assert.calledOnce(networkChangedCalled);
+        navigator.onLine = !onLine;
+        window.dispatchEvent({ type: navigator.onLine ? 'online' : 'offline' });
+        if (connection && connection.type && connection.type !== 'whiz') {
+          const type = connection.type;
+          connection.type = 'bang';
+          connection.dispatchEvent({ type });
+          connection.type = type;
+        }
+        sinon.assert.callCount(onNetworkChanged, shouldCallNetworkChanged ? 1 : 0);
       });
     });
   });
 });
+
+/**
+ * Test the {@link NetworkMonitor}'s #start() or #stop() methods.
+ * @param method - 'start' | 'stop'
+ */
+function testStartOrStop(method) {
+  const eventTargetMethod = {
+    start: 'addEventListener',
+    stop: 'removeEventListener'
+  }[method];
+
+  [undefined, new MockConnection(), new MockConnection('whiz')].forEach(connection => {
+    const events = connection && connection.type ? ['change', 'typechange'] : ['online'];
+    const navigator = connection ? { connection } : {};
+    const window = new EventTarget();
+    const target = connection && connection.type ? connection : window;
+
+    context(`when navigator.connection${connection ? `.type ${connection.type ? 'exists' : 'does not exist'}` : ' does not exist'}`, () => {
+      it(`should ${method === 'start' ? 'listen' : 'stop listening'} to ${events.map(event => `"${event}"`).join(', ')} events on ${target === connection ? 'connection' : 'window'}`, () => {
+        const networkMonitor = new NetworkMonitor(() => {}, { navigator, window });
+        target[eventTargetMethod] = sinon.spy();
+        networkMonitor[method]();
+        events.forEach((event, i) => assert.equal(target[eventTargetMethod].args[i][0], event));
+      });
+    });
+  });
+}
