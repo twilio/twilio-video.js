e9f57655993aa34a3fbb9d650ab543141caea583
Manjesh Malavalli <manjesh.github@gmail.com>
2020-05-05 22:47:15 -0700

    JSDK-2769 Potential fix for "trackSwitched" events test failure.

--- test/integration/spec/localtrackpublication.js
+++ test/integration/spec/localtrackpublication.js
@@ -355,8 +355,6 @@ describe('LocalTrackPublication', function() {
     });
   });
 
-  // eslint-disable-next-line no-warning-comments
-  // TODO: enable these tests when track_priority MSP is available in prod
   (defaults.topology === 'peer-to-peer' ? describe.skip : describe)('#setPriority', function() {
     // eslint-disable-next-line no-invalid-this
     describe('three participant tests', () => {
