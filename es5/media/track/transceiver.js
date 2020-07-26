5824d98b9b5c6c218ef04eabbc0b2e68b703437f
twilio-ci <twilio-ci@twilio.com>
2020-07-26 22:31:34 +0000

    2.7.1-rc1

---
+++ es5/media/track/transceiver.js
@@ -0,0 +1,73 @@
+'use strict';
+
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+var _get = function get(object, property, receiver) { if (object === null) object = Function.prototype; var desc = Object.getOwnPropertyDescriptor(object, property); if (desc === undefined) { var parent = Object.getPrototypeOf(object); if (parent === null) { return undefined; } else { return get(parent, property, receiver); } } else if ("value" in desc) { return desc.value; } else { var getter = desc.get; if (getter === undefined) { return undefined; } return getter.call(receiver); } };
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
+var TrackTransceiver = require('../../transceiver');
+
+/**
+ * A {@link MediaTrackTransceiver} represents either one or more local
+ * RTCRtpSenders, or a single RTCRtpReceiver.
+ * @extends TrackTransceiver
+ * @property {MediaStreamTrack} track
+ */
+
+var MediaTrackTransceiver = function (_TrackTransceiver) {
+  _inherits(MediaTrackTransceiver, _TrackTransceiver);
+
+  /**
+   * Construct a {@link MediaTrackTransceiver}.
+   * @param {Track.ID} id - The MediaStreamTrack ID signaled through RSP/SDP
+   * @param {MediaStreamTrack} mediaStreamTrack
+   */
+  function MediaTrackTransceiver(id, mediaStreamTrack) {
+    _classCallCheck(this, MediaTrackTransceiver);
+
+    var _this = _possibleConstructorReturn(this, (MediaTrackTransceiver.__proto__ || Object.getPrototypeOf(MediaTrackTransceiver)).call(this, id, mediaStreamTrack.kind));
+
+    Object.defineProperties(_this, {
+      _track: {
+        value: mediaStreamTrack,
+        writable: true
+      },
+      enabled: {
+        enumerable: true,
+        get: function get() {
+          return this._track.enabled;
+        }
+      },
+      readyState: {
+        enumerable: true,
+        get: function get() {
+          return this._track.readyState;
+        }
+      },
+      track: {
+        enumerable: true,
+        get: function get() {
+          return this._track;
+        }
+      }
+    });
+    return _this;
+  }
+
+  _createClass(MediaTrackTransceiver, [{
+    key: 'stop',
+    value: function stop() {
+      this.track.stop();
+      _get(MediaTrackTransceiver.prototype.__proto__ || Object.getPrototypeOf(MediaTrackTransceiver.prototype), 'stop', this).call(this);
+    }
+  }]);
+
+  return MediaTrackTransceiver;
+}(TrackTransceiver);
+
+module.exports = MediaTrackTransceiver;
