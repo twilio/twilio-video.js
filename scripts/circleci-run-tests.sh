37cba7e43170d8b7ee192762793c2ab1497da794
Makarand Patwardhan <mpatwardhan@twilio.com>
2020-08-13 10:55:21 -0700

    removed experimental change

--- scripts/circleci-run-tests.sh
+++ scripts/circleci-run-tests.sh
@@ -59,8 +59,5 @@ else
     npm run test:integration
 fi
 
-SLACK_MESSAGE_TEXT="done with ${BROWSER}-${BVER} in ${ENVIRONMENT}"
-curl -X POST -H 'Content-type: application/json' --data '{"text": '\""$SLACK_MESSAGE_TEXT"\"'}' $SLACK_WEBHOOK
-
 echo "Done with Tests!"
 
