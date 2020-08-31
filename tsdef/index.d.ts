39fa51ad2ccc7f42595d3447c2ec58a957e92a81
joma <joma@twilio.com>
2020-08-31 16:15:25 -0700

    JSDK-2951 Added todo comment

--- tsdef/index.d.ts
+++ tsdef/index.d.ts
@@ -2,4 +2,5 @@ import { LocalAudioTrack } from './LocalAudioTrack';
 import { RemoteAudioTrack } from './RemoteAudioTrack';
 import { Track } from './Track';
 
+// NOTE: when adding new declaration files, do not forget to include JSDocs
 export { Track, LocalAudioTrack, RemoteAudioTrack };
