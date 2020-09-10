ab46e708488b6c17e6fa21fc2ab9aa1ef4d19040
Joyce Ma <43423318+PikaJoyce@users.noreply.github.com>
2020-09-09 21:12:10 -0700

    JSDK-2952 TypeScript Definitions: DataTrack (#1164)
    * JSDK-2952 LocalDataTrack and RemoteDataTrack TS definitions
    
    Co-authored-by: joma <joma@twilio.com>

---
+++ tsdef/LocalDataTrackOptions.d.ts
@@ -0,0 +1,5 @@
+export interface LocalDataTrackOptions {
+  maxPacketLifeTime?: number | null;
+  maxRetransmits?: number | null;
+  ordered?: boolean;
+}
--- tsdef/index.d.ts
+++ tsdef/index.d.ts
@@ -1,6 +1,8 @@
 import { LocalAudioTrack } from './LocalAudioTrack';
+import { LocalDataTrack } from './LocalDataTrack';
 import { RemoteAudioTrack } from './RemoteAudioTrack';
+import { RemoteDataTrack } from './RemoteDataTrack';
 import { Track } from './Track';
 
 // NOTE: when adding new declaration files, do not forget to include JSDocs
-export { Track, LocalAudioTrack, RemoteAudioTrack };
+export { Track, LocalAudioTrack, RemoteAudioTrack, LocalDataTrack, RemoteDataTrack };
