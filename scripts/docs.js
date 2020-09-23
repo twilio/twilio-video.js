f66c81ea38389fee43d395001eb4499f0b6aa00a
joma <joma@twilio.com>
2020-09-23 14:01:52 -0700

    JSDK-2985 Changes from develop branch

--- scripts/docs.js
+++ scripts/docs.js
@@ -14,6 +14,8 @@ const publicClasses = [
   'lib/createlocaltrack.js',
   'lib/createlocaltracks.js',
   'lib/index.js',
+  'lib/preflight/preflight.js',
+  'lib/preflight/preflighttest.js',
   'lib/room.js',
   'lib/media/track/index.js',
   'lib/media/track/audiotrack.js',
@@ -55,6 +57,7 @@ const publicClasses = [
   'lib/stats/remoteaudiotrackstats.js',
   'lib/stats/remotevideotrackstats.js',
   'lib/util/twilio-video-errors.js',
+  'lib/util/timemeasurement.js',
   'lib/util/twilioerror.js'
 ];
 
