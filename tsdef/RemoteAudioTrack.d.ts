aae6b0c051f4222d3e1adc1f7096961c4467b16b
joma <joma@twilio.com>
2020-09-22 18:06:06 -0700

    Removing JSDocs from TS Definitions

--- tsdef/RemoteAudioTrack.d.ts
+++ tsdef/RemoteAudioTrack.d.ts
@@ -1,58 +1,9 @@
 import { AudioTrack } from './AudioTrack';
 import { Track } from './Track';
 
-/**
- * A {@link RemoteAudioTrack} represents an {@link AudioTrack} published to a
- * {@link Room} by a {@link RemoteParticipant}.
- * @extends AudioTrack
- * @property {boolean} isEnabled - Whether the {@link RemoteAudioTrack} is enabled
- * @property {boolean} isSwitchedOff - Whether the {@link RemoteAudioTrack} is switched off
- * @property {Track.SID} sid - The {@link RemoteAudioTrack}'s SID
- * @property {?Track.Priority} priority - The subscribe priority of the {@link RemoteAudioTrack}
- * @emits RemoteAudioTrack#disabled
- * @emits RemoteAudioTrack#enabled
- * @emits RemoteAudioTrack#started
- * @emits RemoteAudioTrack#switchedOff
- * @emits RemoteAudioTrack#switchedOn
- */
 export class RemoteAudioTrack extends AudioTrack {
   sid: Track.SID;
   priority: Track.Priority;
   isSwitchedOff: boolean;
   isEnabled: boolean;
 }
-
-/**
- * The {@link RemoteAudioTrack} was disabled, i.e. "muted".
- * @param {RemoteAudioTrack} track - The {@link RemoteAudioTrack} that was
- *   disabled
- * @event RemoteAudioTrack#disabled
- */
-
-/**
- * The {@link RemoteAudioTrack} was enabled, i.e. "unmuted".
- * @param {RemoteAudioTrack} track - The {@link RemoteAudioTrack} that was
- *   enabled
- * @event RemoteAudioTrack#enabled
- */
-
-/**
- * The {@link RemoteAudioTrack} started. This means there is enough audio data
- * to begin playback.
- * @param {RemoteAudioTrack} track - The {@link RemoteAudioTrack} that started
- * @event RemoteAudioTrack#started
- */
-
-/**
- * A {@link RemoteAudioTrack} was switched off.
- * @param {RemoteAudioTrack} track - The {@link RemoteAudioTrack} that was
- *   switched off
- * @event RemoteAudioTrack#switchedOff
- */
-
-/**
- * A {@link RemoteAudioTrack} was switched on.
- * @param {RemoteAudioTrack} track - The {@link RemoteAudioTrack} that was
- *   switched on
- * @event RemoteAudioTrack#switchedOn
- */
