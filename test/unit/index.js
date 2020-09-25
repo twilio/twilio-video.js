145ac3d4b3fb51802cd275e8cdf91df2f15df4aa
Makarand Patwardhan <mpatwardhan@twilio.com>
2020-09-25 10:17:40 -0700

    revert preflight code (#1192)

--- test/unit/index.js
+++ test/unit/index.js
@@ -5,7 +5,6 @@ if (typeof window === 'undefined') {
 }
 
 require('./spec/connect');
-require('./spec/preflight');
 require('./spec/createlocaltrack');
 require('./spec/createlocaltracks');
 require('./spec/encodingparameters');
