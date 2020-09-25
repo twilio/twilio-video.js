f866f984d13b1010423ecbe8a3736d00e5af521d
joma <joma@twilio.com>
2020-09-25 16:50:06 -0700

    JSDK-2953 Added Video Track Definitions

---
+++ tsdef/VideoTrack.d.ts
@@ -0,0 +1,25 @@
+import { Track } from './Track';
+
+export namespace VideoTrack {
+  interface Dimensions {
+      width: number | null;
+      height: number | null;
+  }
+}
+
+export class VideoTrack extends Track {
+  isStarted: boolean;
+  isEnabled: boolean;
+  dimensions: VideoTrack.Dimensions;
+  kind: 'video';
+  mediaStreamTrack: MediaStreamTrack;
+
+  _attachments?: HTMLMediaElement[];
+
+  attach(element?: HTMLMediaElement | string): HTMLVideoElement;
+  detach(element?: HTMLMediaElement | string): HTMLMediaElement[];
+
+  on(event: 'disabled', listener: (track: this) => void): this;
+  on(event: 'enabled', listener: (track: this) => void): this;
+  on(event: 'started', listener: (track: this) => void): this;
+}
