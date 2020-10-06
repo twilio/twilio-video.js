4fe135ffe1040bd6e1bde2740645e69e1ce0e521
Makarand Patwardhan <mpatwardhan@twilio.com>
2020-10-06 15:45:04 -0700

    docker login + test for chrome bugs

--- test/integration/index.js
+++ test/integration/index.js
@@ -3,6 +3,7 @@
 require('./spec/docker/reconnection');
 require('./spec/docker/docker');
 require('./spec/connect');
+require('./spec/external');
 require('./spec/localparticipant');
 require('./spec/localtracks');
 require('./spec/localtrackpublication');
