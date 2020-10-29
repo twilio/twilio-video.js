ea9a557dfb8b1698fdee5f5f09279f3218e71c6e
joma <joma@twilio.com>
2020-10-29 15:50:59 -0700

    JSDK-3018 Moving LogLevels, LocalTrackOptions, and Members into types.d.ts

--- tsdef/LocalTrackOptions.d.ts
+++
@@ -1,7 +0,0 @@
-import { LogLevel } from './Members';
-import { LogLevels } from './LogLevels';
-
-export interface LocalTrackOptions {
-  logLevel: LogLevel | LogLevels;
-  name?: string;
-}
--- tsdef/types.d.ts
+++ tsdef/types.d.ts
@@ -10,6 +10,8 @@ import { RemoteAudioTrackPublication } from './RemoteAudioTrackPublication';
 import { RemoteDataTrack } from './RemoteDataTrack';
 import { RemoteVideoTrack } from './RemoteVideoTrack';
 import { RemoteVideoTrackPublication } from './RemoteVideoTrackPublication';
+import { Track } from './Track';
+import { VideoTrack } from './VideoTrack';
 
 export type LocalTrack = LocalAudioTrack | LocalVideoTrack | LocalDataTrack;
 export type RemoteTrack = RemoteAudioTrack | RemoteVideoTrack | RemoteDataTrack;
@@ -67,4 +69,88 @@ export class NetworkQualityLatencyStats {
 export interface NetworkQualityConfiguration {
   local?: NetworkQualityVerbosity;
   remote?: NetworkQualityVerbosity;
+}
+
+export type AudioLevel = number;
+
+export type AudioCodec = 'isac' | 'opus' | 'PCMA' | 'PCMU';
+export type VideoCodec = 'H264' | 'VP8' | 'VP9';
+
+export type LogLevel = 'debug' | 'info' | 'warn' | 'error' | 'off';
+export interface LogLevels {
+  default: LogLevel;
+  media: LogLevel;
+  signaling: LogLevel;
+  webrtc: LogLevel;
+}
+
+export type TrackSwitchOffMode = 'detected' | 'predicted' | 'disabled';
+export type BandwidthProfileMode = 'grid' | 'collaboration' | 'presentation';
+
+export interface VideoRenderDimensions {
+  high?: VideoTrack.Dimensions;
+  low?: VideoTrack.Dimensions;
+  standard?: VideoTrack.Dimensions;
+}
+
+export interface VideoBandwidthProfileOptions {
+  dominantSpeakerPriority?: Track.Priority;
+  maxSubscriptionBitrate?: number;
+  maxTracks?: number;
+  mode?: BandwidthProfileMode;
+  renderDimensions?: VideoRenderDimensions;
+  trackSwitchOffMode?: TrackSwitchOffMode;
+}
+
+export interface BandwidthProfileOptions {
+  video?: VideoBandwidthProfileOptions;
+}
+
+export interface VideoCodecSettings {
+  codec: VideoCodec;
+}
+
+export interface VP8CodecSettings extends VideoCodecSettings {
+  codec: 'VP8';
+  simulcast?: boolean;
+}
+
+export interface LocalTrackOptions {
+  logLevel: LogLevel | LogLevels;
+  name?: string;
+}
+
+export interface ConnectOptions {
+  abortOnIceServersTimeout?: boolean;
+  audio?: boolean | CreateLocalTrackOptions;
+  automaticSubscription?: boolean;
+  bandwidthProfile?: BandwidthProfileOptions;
+  dominantSpeaker?: boolean;
+  dscpTagging?: boolean;
+  enableDscp?: boolean;
+  iceServers?: RTCIceServer[];
+  iceServersTimeout?: number;
+  iceTransportPolicy?: RTCIceTransportPolicy;
+  insights?: boolean;
+  maxAudioBitrate?: number | null;
+  maxVideoBitrate?: number | null;
+  name?: string | null;
+  networkQuality?: boolean | NetworkQualityConfiguration;
+  region?: 'au1' | 'br1' | 'ie1' | 'de1' | 'jp1' | 'sg1' | 'us1' | 'us2' | 'gll';
+  preferredAudioCodecs?: AudioCodec[];
+  preferredVideoCodecs?: Array<VideoCodec | VideoCodecSettings | VP8CodecSettings>;
+  logLevel?: LogLevel | LogLevels;
+  tracks?: LocalTrack[] | MediaStreamTrack[];
+  video?: boolean | CreateLocalTrackOptions;
+}
+
+export interface CreateLocalTrackOptions extends MediaTrackConstraints {
+  logLevel?: LogLevel | LogLevels;
+  name?: string;
+  workaroundWebKitBug180748?: boolean;
+}
+export interface CreateLocalTracksOptions {
+  audio?: boolean | CreateLocalTrackOptions;
+  logLevel?: LogLevel | LogLevels;
+  video?: boolean | CreateLocalTrackOptions;
 }
