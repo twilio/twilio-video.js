e275fbdc521c0659e63e965ba3e7825be7c301c6
joma <joma@twilio.com>
2020-10-29 15:52:18 -0700

    JSDK-3018 Creating definitions for top level exports and Room

--- tsdef/Room.d.ts
+++ tsdef/Room.d.ts
@@ -1,53 +1,53 @@
-// import { MediaConnectionError, SignalingConnectionDisconnectedError } from './error';
-// import { EventEmitter } from 'events';
-// import { LocalParticipant } from './LocalParticipant';
-// import { Participant } from './Participant';
-// import { RemoteParticipant } from './RemoteParticipant';
-// import { RemoteTrack } from './types';
-// import { RemoteTrackPublication } from './RemoteTrackPublication';
-// import { RemoteVideoTrack } from './RemoteVideoTrack';
-// import { StatsReport } from './stats';
-// import { Track } from './Track';
-// import { TwilioError } from './TwilioError';
+import { MediaConnectionError, SignalingConnectionDisconnectedError } from './error';
+import { EventEmitter } from 'events';
+import { LocalParticipant } from './LocalParticipant';
+import { Participant } from './Participant';
+import { RemoteParticipant } from './RemoteParticipant';
+import { RemoteTrack } from './types';
+import { RemoteTrackPublication } from './RemoteTrackPublication';
+import { RemoteVideoTrack } from './RemoteVideoTrack';
+import { StatsReport } from './stats';
+import { Track } from './Track';
+import { TwilioError } from './TwilioError';
 
-// export namespace Room {
-//   type SID = string;
-// }
+export namespace Room {
+  type SID = string;
+}
 
-// export class Room extends EventEmitter {
-//   dominantSpeaker: RemoteParticipant | null;
-//   isRecording: boolean;
-//   localParticipant: LocalParticipant;
-//   mediaRegion: string;
-//   name: string;
-//   participants: Map<Participant.SID, RemoteParticipant>;
-//   sid: Room.SID;
-//   state: string;
+export class Room extends EventEmitter {
+  dominantSpeaker: RemoteParticipant | null;
+  isRecording: boolean;
+  localParticipant: LocalParticipant;
+  mediaRegion: string;
+  name: string;
+  participants: Map<Participant.SID, RemoteParticipant>;
+  sid: Room.SID;
+  state: string;
 
-//   disconnect(): Room;
-//   getStats(): Promise<StatsReport[]>;
+  disconnect(): Room;
+  getStats(): Promise<StatsReport[]>;
 
-//   on(event: 'disconnected', listener: (room: Room, error: TwilioError) => void): this;
-//   on(event: 'dominantSpeakerChanged', listener: (dominantSpeaker: RemoteParticipant) => void): this;
-//   on(event: 'participantConnected', listener: (participant: RemoteParticipant) => void): this;
-//   on(event: 'participantDisconnected', listener: (participant: RemoteParticipant) => void): this;
-//   on(event: 'participantReconnected', listener: (participant: RemoteParticipant) => void): this;
-//   on(event: 'participantReconnecting', listener: (participant: RemoteParticipant) => void): this;
-//   on(event: 'reconnected', listener: () => void): this;
-//   on(event: 'reconnecting', listener: (error: MediaConnectionError | SignalingConnectionDisconnectedError) => void): this;
-//   on(event: 'recordingStarted', listener: () => void): this;
-//   on(event: 'recordingStopped', listener: () => void): this;
-//   on(event: 'trackDimensionsChanged', listener: (track: RemoteVideoTrack, participant: RemoteParticipant) => void): this;
-//   on(event: 'trackDisabled', listener: (publication: RemoteTrackPublication, participant: RemoteParticipant) => void): this;
-//   on(event: 'trackEnabled', listener: (publication: RemoteTrackPublication, participant: RemoteParticipant) => void): this;
-//   on(event: 'trackMessage', listener: (data: string | ArrayBuffer, track: RemoteVideoTrack, participant: RemoteParticipant) => void): this;
-//   on(event: 'trackPublished', listener: (publication: RemoteTrackPublication, participant: RemoteParticipant) => void): this;
-//   on(event: 'trackPublishPriorityChanged', listener: (priority: Track.Priority, publication: RemoteTrackPublication, participant: RemoteParticipant) => void): this;
-//   on(event: 'trackStarted', listener: (track: RemoteTrack, participant: RemoteParticipant) => void): this;
-//   on(event: 'trackSubscribed', listener: (track: RemoteTrack, publication: RemoteTrackPublication, participant: RemoteParticipant) => void): this;
-//   on(event: 'trackSubscriptionFailed', listener: (error: TwilioError, publication: RemoteTrackPublication, participant: RemoteParticipant) => void): this;
-//   on(event: 'trackSwitchedOff', listener: (track: RemoteTrack, publication: RemoteTrackPublication, participant: RemoteParticipant) => void): this;
-//   on(event: 'trackSwitchedOn', listener: (track: RemoteTrack, publication: RemoteTrackPublication, participant: RemoteParticipant) => void): this;
-//   on(event: 'trackUnpublished', listener: (publication: RemoteTrackPublication, participant: RemoteParticipant) => void): this;
-//   on(event: 'trackUnsubscribed', listener: (track: RemoteTrack, publication: RemoteTrackPublication, participant: RemoteParticipant) => void): this;
-// }
+  on(event: 'disconnected', listener: (room: Room, error: TwilioError) => void): this;
+  on(event: 'dominantSpeakerChanged', listener: (dominantSpeaker: RemoteParticipant) => void): this;
+  on(event: 'participantConnected', listener: (participant: RemoteParticipant) => void): this;
+  on(event: 'participantDisconnected', listener: (participant: RemoteParticipant) => void): this;
+  on(event: 'participantReconnected', listener: (participant: RemoteParticipant) => void): this;
+  on(event: 'participantReconnecting', listener: (participant: RemoteParticipant) => void): this;
+  on(event: 'reconnected', listener: () => void): this;
+  on(event: 'reconnecting', listener: (error: MediaConnectionError | SignalingConnectionDisconnectedError) => void): this;
+  on(event: 'recordingStarted', listener: () => void): this;
+  on(event: 'recordingStopped', listener: () => void): this;
+  on(event: 'trackDimensionsChanged', listener: (track: RemoteVideoTrack, participant: RemoteParticipant) => void): this;
+  on(event: 'trackDisabled', listener: (publication: RemoteTrackPublication, participant: RemoteParticipant) => void): this;
+  on(event: 'trackEnabled', listener: (publication: RemoteTrackPublication, participant: RemoteParticipant) => void): this;
+  on(event: 'trackMessage', listener: (data: string | ArrayBuffer, track: RemoteVideoTrack, participant: RemoteParticipant) => void): this;
+  on(event: 'trackPublished', listener: (publication: RemoteTrackPublication, participant: RemoteParticipant) => void): this;
+  on(event: 'trackPublishPriorityChanged', listener: (priority: Track.Priority, publication: RemoteTrackPublication, participant: RemoteParticipant) => void): this;
+  on(event: 'trackStarted', listener: (track: RemoteTrack, participant: RemoteParticipant) => void): this;
+  on(event: 'trackSubscribed', listener: (track: RemoteTrack, publication: RemoteTrackPublication, participant: RemoteParticipant) => void): this;
+  on(event: 'trackSubscriptionFailed', listener: (error: TwilioError, publication: RemoteTrackPublication, participant: RemoteParticipant) => void): this;
+  on(event: 'trackSwitchedOff', listener: (track: RemoteTrack, publication: RemoteTrackPublication, participant: RemoteParticipant) => void): this;
+  on(event: 'trackSwitchedOn', listener: (track: RemoteTrack, publication: RemoteTrackPublication, participant: RemoteParticipant) => void): this;
+  on(event: 'trackUnpublished', listener: (publication: RemoteTrackPublication, participant: RemoteParticipant) => void): this;
+  on(event: 'trackUnsubscribed', listener: (track: RemoteTrack, publication: RemoteTrackPublication, participant: RemoteParticipant) => void): this;
+}
--- tsdef/twilio-video.d.ts
+++ tsdef/twilio-video.d.ts
@@ -1,8 +1,12 @@
-// import { Room } from './Room';
+import { ConnectOptions, CreateLocalTrackOptions, CreateLocalTracksOptions, LocalTrack } from './types';
+import { LocalAudioTrack } from './LocalAudioTrack';
+import { LocalTrackStats } from './stats';
+import { LocalVideoTrack } from './LocalVideoTrack';
+import { Room } from './Room';
 
 
-// export function connect(token: string, options?: ConnectOptions): Promise<Room>;
-// export function createLocalAudioTrack(options?: CreateLocalTrackOptions): Promise<LocalAudioTrack>;
-// export function createLocalTracks(options?: CreateLocalTracksOptions): Promise<LocalTrack[]>;
-// export function createLocalVideoTrack(options?: CreateLocalTrackOptions): Promise<LocalVideoTrack>;
-// export function rewriteLocalTrackIds(room: Room, trackStats: LocalTrackStats[]): LocalTrackStats[];
+export function connect(token: string, options?: ConnectOptions): Promise<Room>;
+export function createLocalAudioTrack(options?: CreateLocalTrackOptions): Promise<LocalAudioTrack>;
+export function createLocalTracks(options?: CreateLocalTracksOptions): Promise<LocalTrack[]>;
+export function createLocalVideoTrack(options?: CreateLocalTrackOptions): Promise<LocalVideoTrack>;
+export function rewriteLocalTrackIds(room: Room, trackStats: LocalTrackStats[]): LocalTrackStats[];
