c4a009d39fd44ea9bd690ee897019bbd1298ba9a
joma <joma@twilio.com>
2020-11-10 12:35:19 -0800

    JSDK-3018 Removing specific error codes

--- tsdef/TwilioError.d.ts
+++ tsdef/TwilioError.d.ts
@@ -4,13 +4,3 @@ export class TwilioError extends Error {
 
   toString(): string;
 }
-
-export interface MediaConnectionError extends TwilioError {
-  code: 53405;
-  message: 'Media connection failed';
-}
-
-export interface SignalingConnectionDisconnectedError extends TwilioError {
-  code: 53001;
-  message: 'Signaling connection disconnected';
-}
