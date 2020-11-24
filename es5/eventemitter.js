2dc2f7ac50cc766db50c4e1b4b90a8f004e7702f
twilio-ci <twilio-ci@twilio.com>
2020-11-24 22:38:14 +0000

    2.9.0-dev

--- es5/eventemitter.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-var _require = require('events'),
-    EventEmitter = _require.EventEmitter;
-
-var _require2 = require('./util'),
-    hidePrivateAndCertainPublicPropertiesInClass = _require2.hidePrivateAndCertainPublicPropertiesInClass;
-
-module.exports = hidePrivateAndCertainPublicPropertiesInClass(EventEmitter, ['domain']);
