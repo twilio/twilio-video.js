1719feba90cf01dc98829fbfab30b9f0589d3ff2
Makarand Patwardhan <mpatwardhan@twilio.com>
2020-12-15 16:36:17 -0800

    send warning events

--- test/unit/spec/signaling/v2/peerconnection.js
+++ test/unit/spec/signaling/v2/peerconnection.js
@@ -2289,6 +2289,7 @@ function makePeerConnectionV2(options) {
     RTCPeerConnection: options.RTCPeerConnection,
     RTCSessionDescription: identity,
     isChromeScreenShareTrack: options.isChromeScreenShareTrack,
+    eventObserver: options.eventObserver || { emit: sinon.spy() },
     isRTCRtpSenderParamsSupported: options.isRTCRtpSenderParamsSupported,
     sessionTimeout: options.sessionTimeout,
     setBitrateParameters: options.setBitrateParameters,
