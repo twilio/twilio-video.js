bbbbfe3e0aff2b6eb05a39f9ba16b1763c77b4b8
Joyce Ma <43423318+PikaJoyce@users.noreply.github.com>
2021-03-10 20:18:38 -0800

    VIDEO-4298-IntersectionObserver Unit Tests (#1412)
    * IntersectionObserver Unit Tests
    
    Co-authored-by: joma <joma@twilio.com>

--- test/unit/spec/media/track/remotevideotrack.js
+++ test/unit/spec/media/track/remotevideotrack.js
@@ -8,6 +8,7 @@ const MediaTrackReceiver = require('../../../../../lib/media/track/receiver');
 const RemoteVideoTrack = require('../../../../../lib/media/track/remotevideotrack');
 const { FakeMediaStreamTrack } = require('../../../../lib/fakemediastream');
 const documentVisibilityMonitor = require('../../../../../lib/util/documentvisibilitymonitor');
+const NullIntersectionObserver = require('../../../../../lib/util/nullintersectionobserver');
 
 const kind = 'video';
 const RemoteTrack = RemoteVideoTrack;
@@ -79,8 +80,79 @@ describe('RemoteVideoTrack', () => {
       });
     });
   });
-});
 
+  describe('IntersectionObserver', () => {
+    let track;
+    let el;
+    let observeSpy;
+    let unobserveSpy;
+    let setRenderHintSpy;
+
+    before(() => {
+      global.document = global.document || new Document();
+      el = document.createElement('video');
+      setRenderHintSpy = sinon.spy();
+      track = makeTrack({ id: 'foo', sid: 'bar', kind, isSwitchedOff: false, setRenderHint: setRenderHintSpy, isEnabled: true, options: { IntersectionObserver: NullIntersectionObserver }, RemoteTrack });
+      observeSpy = sinon.spy(NullIntersectionObserver.prototype, 'observe');
+      unobserveSpy = sinon.spy(NullIntersectionObserver.prototype, 'unobserve');
+    });
+
+    after(() => {
+      observeSpy.restore();
+      unobserveSpy.restore();
+      if (global.document instanceof Document && kind === 'video') {
+        delete global.document;
+      }
+    });
+
+    context('when an element is attached', () => {
+      beforeEach(() => {
+        track.attach(el);
+      });
+
+      it('IntersectionObserver observe is called', () => {
+        sinon.assert.callCount(observeSpy, 1);
+      });
+
+      it('_setRenderHint gets called with { enable: true }', () => {
+        sinon.assert.calledWith(setRenderHintSpy, sinon.match.has('enabled', true));
+        sinon.assert.calledWith(setRenderHintSpy, sinon.match.has('renderDimensions', { height: sinon.match.any, width: sinon.match.any }));
+      });
+    });
+
+    context('when an element is detached', () => {
+      beforeEach(() => {
+        track.detach(el);
+      });
+
+      it('IntersectionObserver unobserve is called', () => {
+        sinon.assert.callCount(unobserveSpy, 1);
+      });
+
+      it(' _setRenderHint gets called with { enable: false }', () => {
+        sinon.assert.calledWith(setRenderHintSpy, { enabled: false });
+      });
+    });
+
+    context('when an element is', () => {
+      before(() => {
+        track.attach(el);
+        setRenderHintSpy.reset();
+      });
+
+      it('invisible, _setRenderHint gets called with { enable: false }', () => {
+        track._intersectionObserver.makeInvisible(el);
+        sinon.assert.calledWith(setRenderHintSpy, { enabled: false });
+      });
+
+      it('visible, _setRenderHint gets called with { enable: true }', () => {
+        track._intersectionObserver.makeVisible(el);
+        sinon.assert.calledWith(setRenderHintSpy, sinon.match.has('enabled', true));
+        sinon.assert.calledWith(setRenderHintSpy, sinon.match.has('renderDimensions', { height: sinon.match.any, width: sinon.match.any }));
+      });
+    });
+  });
+});
 
 function makeTrack({ id, sid, kind, isEnabled, options, RemoteTrack, setPriority, setRenderHint, isSwitchedOff }) {
   const emptyFn = () => undefined;
@@ -89,11 +161,6 @@ function makeTrack({ id, sid, kind, isEnabled, options, RemoteTrack, setPriority
   isSwitchedOff = !!isSwitchedOff;
   const mediaStreamTrack = new FakeMediaStreamTrack(kind);
   const mediaTrackReceiver = new MediaTrackReceiver(id, mediaStreamTrack);
-  class FakeIntersectionObserver {
-    constructor() {}
-    observe() {}
-    unobserve() {}
-  }
-  options.IntersectionObserver = FakeIntersectionObserver;
+  options.IntersectionObserver = NullIntersectionObserver;
   return new RemoteTrack(sid, mediaTrackReceiver, isEnabled, isSwitchedOff, setPriority, setRenderHint, options);
 }
