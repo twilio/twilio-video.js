85e54920b5f7f044e07fb85746fa41ec598aeeec
twilio-ci <twilio-ci@twilio.com>
2021-06-16 17:45:02 +0000

    2.15.1-dev

--- es5/media/track/es5/index.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-module.exports = {
-  LocalAudioTrack: require('./localaudiotrack'),
-  LocalVideoTrack: require('./localvideotrack'),
-  LocalDataTrack: require('./localdatatrack')
-};
