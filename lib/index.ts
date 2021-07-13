44bb53747684741bf78a8becb6ec1480efb36b4b
Makarand Patwardhan <mpatwardhan@twilio.com>
2021-07-13 15:08:09 -0700

    build before unit tests

--- lib/index.ts
+++ lib/index.ts
@@ -35,10 +35,24 @@ function createLocalVideoTrack(options?: CreateLocalTrackOptions): Promise<Local
   return internals.createLocalVideoTrack(options);
 }
 
+/**
+ * @module twilio-video
+ * @property {boolean} isSupported - true if the current browser is officially
+ *   supported by twilio-video.js; In this context, "supported" means that
+ *   twilio-video.js has been extensively tested with this browser; This
+ *   <a href="https://www.twilio.com/docs/video/javascript#supported-browsers" target="_blank">table</a>
+ *   specifies the list of officially supported browsers.
+ *
+ * @property {object} Logger - The <a href="https://www.npmjs.com/package/loglevel" target="_blank">loglevel</a>
+ *    module used by the SDK. Use this object to access the internal loggers and perform actions as defined by the
+ *   <a href="https://www.npmjs.com/package/loglevel" target="_blank">loglevel</a> APIs.
+ *   See [connect](#.connect) for examples.
+ *
+ * @property {string} version - current version of twilio-video.js.
+ */
+
 const isSupported: boolean = internals.isSupported;
-
-const version: boolean = internals.version
-
+const version: boolean = internals.version;
 const Logger: Log.RootLogger = internals.Logger;
 const LocalAudioTrack = internals.LocalAudioTrack;
 const LocalVideoTrack = internals.LocalVideoTrack;
@@ -57,20 +71,4 @@ export {
   LocalVideoTrack,
   LocalDataTrack
 };
-
-/**
- * @module twilio-video
- * @property {boolean} isSupported - true if the current browser is officially
- *   supported by twilio-video.js; In this context, "supported" means that
- *   twilio-video.js has been extensively tested with this browser; This
- *   <a href="https://www.twilio.com/docs/video/javascript#supported-browsers" target="_blank">table</a>
- *   specifies the list of officially supported browsers.
- *
- * @property {object} Logger - The <a href="https://www.npmjs.com/package/loglevel" target="_blank">loglevel</a>
- *    module used by the SDK. Use this object to access the internal loggers and perform actions as defined by the
- *   <a href="https://www.npmjs.com/package/loglevel" target="_blank">loglevel</a> APIs.
- *   See [connect](#.connect) for examples.
- *
- * @property {string} version - current version of twilio-video.js.
- */
 
