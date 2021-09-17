2beb2039151bdd58deb6dd6b63073f64c2128e20
Makarand Patwardhan <mpatwardhan@twilio.com>
2021-09-17 15:44:05 -0700

    minor

--- test/unit/spec/signaling/v2/peerconnection.js
+++ test/unit/spec/signaling/v2/peerconnection.js
@@ -1211,12 +1211,11 @@ describe('PeerConnectionV2', () => {
 
       function itShouldNotSetResolutionScale() {
         if (isRTCRtpSenderParamsSupported) {
-          it('should set RTCRtpEncodingParameters.scaleResolutionDownBy for any video senders', () => {
+          it('should not set RTCRtpEncodingParameters.scaleResolutionDownBy for any video senders', () => {
             test.pc.getSenders().forEach(sender => {
               if (sender.track.kind === 'video') {
                 sinon.assert.calledWith(sender.setParameters, sinon.match(({ encodings }) => {
-                  const found = encodings.find(encoding => typeof encoding.scaleResolutionDownBy !== 'undefined');
-                  return !found;
+                  return !encodings.find(encoding => typeof encoding.scaleResolutionDownBy !== 'undefined');
                 }));
               }
             });
