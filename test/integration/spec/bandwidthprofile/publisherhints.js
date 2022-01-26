10f2da61994bbc0ab7f68633a40e90806f543ab1
joma <joma@twilio.com>
2022-01-25 16:36:49 -0800

    Adding a couple more pointers to es5/index

--- test/integration/spec/bandwidthprofile/publisherhints.js
+++ test/integration/spec/bandwidthprofile/publisherhints.js
@@ -5,8 +5,7 @@
 const assert = require('assert');
 const { video: createLocalVideoTrack } = require('../../../../es5/createlocaltrack');
 const defaults = require('../../../lib/defaults');
-const { Logger } = require('../../../../es5');
-const connect = require('../../../../es5/connect');
+const { Logger, connect } = require('../../../../es5');
 const { createRoom, completeRoom } = require('../../../lib/rest');
 const getToken = require('../../../lib/token');
 const { isFirefox } = require('../../../lib/guessbrowser');
