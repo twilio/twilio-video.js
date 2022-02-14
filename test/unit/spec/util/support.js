10ee95ffb0f8cd6338c8ad24b3f54472eef84703
joma <joma@twilio.com>
2022-02-14 11:44:50 -0800

    Added isSupported unit tests and a CHANGELOG entry

--- test/unit/spec/util/support.js
+++ test/unit/spec/util/support.js
@@ -82,6 +82,29 @@ describe('isSupported', () => {
       'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13 Safari/605.1.15'
     ],
     [
+      'Safari WKWebView - iPhone',
+      'Mozilla/5.0 (iPhone; CPU iPhone OS 15_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko)',
+    ],
+    [
+      'Safari WKWebView - iPad',
+      'Mozilla/5.0 (iPad; CPU OS 15_3 like Mac OS X) AppleWebkit/605.1.15 (KHTML, like Gecko) Mobile/15E148',
+    ],
+    [
+      'Safari iOS - Instagram',
+      'Mozilla/5.0 (iPhone; CPU iPhone OS 15_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 Instagram 220.0.0.8.117 (iPhone12,3; iOS 15_3; en_US; en; scale=3.00; 1125x2436; 347566818) NW/3',
+      'safari'
+    ],
+    [
+      'Safari iOS - Snapchat',
+      'Mozilla/5.0 (iPhone; CPU iPhone OS 15_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.3 Mobile/15E148 Snapchat/11.64.0.38 (like Safari/8612.4.9.0.3, panda)',
+      'safari'
+    ],
+    [
+      'Safari iOS - Slack ',
+      'Mozilla/5.0 (iPhone; CPU iPhone OS 15_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.3 Mobile/15E148 Safari/604.1',
+      'safari'
+    ],
+    [
       'Edge (Chromium) on Mac',
       'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.212 Safari/537.36 Edg/90.0.818.66',
       { runtime: {} }
