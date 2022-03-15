b5521228ac16f8e108b69ee0350943787bde0636
Makarand Patwardhan <mpatwardhan@twilio.com>
2022-03-15 14:20:40 -0700

    maintain processor

--- lib/media/track/krisplocalaudiotrack.ts
+++ lib/media/track/krisplocalaudiotrack.ts
@@ -12,12 +12,11 @@ const LocalAudioTrack = require('./localaudiotrack');
  * an external noise cancellation SDK
  */
 export class KrispLocalAudioTrack extends LocalAudioTrack {
-  _processor: AudioProcessor|null;
+  _processor: AudioProcessor;
 
   constructor(mediaStreamTrack: MediaStreamTrack, processor: AudioProcessor, options: CreateLocalTrackOptions) {
     super(mediaStreamTrack, options);
     this._processor = processor;
-    this._log.info('LocalAudioTrack options: ', options);
   }
 
   private toString() : string {
@@ -29,23 +28,18 @@ export class KrispLocalAudioTrack extends LocalAudioTrack {
     this._log.debug('_reacquireTrack: ', constraints);
 
     // disconnect the processor.
-    const processorWasEnabled = this._processor?.isEnabled();
-    if (this._processor) {
-      this._processor.disconnect();
-    }
+    const processorWasEnabled = this._processor.isEnabled();
+    this._processor.disconnect();
 
     const track = await super._reacquireTrack.call(this, constraints);
-    if (this._processor) {
-      this._log.debug('_maybeProcessTrack: ', track);
-      const processedTrack = this._processor.connect(track);
-      if (processorWasEnabled) {
-        this._processor.enable();
-      } else {
-        this._processor.disable();
-      }
-      return processedTrack;
+    this._log.debug('_maybeProcessTrack: ', track);
+    const processedTrack = this._processor.connect(track);
+    if (processorWasEnabled) {
+      this._processor.enable();
+    } else {
+      this._processor.disable();
     }
-    return track;
+    return processedTrack;
   }
 
   /**
@@ -53,31 +47,14 @@ export class KrispLocalAudioTrack extends LocalAudioTrack {
    * if the track was enabled for krisp processing.
    */
   enableKrisp() : void {
-    if (this._processor) {
-      this._log.debug('enableKrisp: ');
-      this._processor.enable();
-    }
-  }
-
-  /**
-   * disables krisp processing permanently for the track.
-   */
-  _disableKrispInternal(): void {
-    if (this._processor) {
-      this._log.debug('_disableKrispInternal: ');
-      this._processor.disable();
-    }
-    this._processor = null;
+    this._processor.enable();
   }
 
   /**
    * disables krisp processing
    */
   disableKrisp() : void {
-    if (this._processor) {
-      this._log.debug('disableKrisp: ');
-      this._processor.disable();
-    }
+    this._processor.disable();
   }
 }
 
