71b27557e1fc26e436135161dae096e12a407e39
twilio-ci <twilio-ci@twilio.com>
2022-05-03 20:04:08 +0000

    2.22.0-dev

--- es5/dynamicImport.js
+++
@@ -1,9 +0,0 @@
-// typescript has an issue that it compiles await import into require when module is set to commonjs
-// https://github.com/microsoft/TypeScript/issues/43329
-
-// To workaround this issue we need following code to be not compiled by typescript.
-
-'use strict';
-module.exports = function(module) {
-  return import(/* webpackIgnore: true */ module);
-};
