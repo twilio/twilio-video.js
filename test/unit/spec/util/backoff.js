c22664172fd84c646a69c86f474f5bdb5d41f940
Joyce Ma <joma@twilio.com>
2022-05-10 12:31:28 -0700

    Moving passing of function into start method and cleaning up unit tests

--- test/unit/spec/util/backoff.js
+++ test/unit/spec/util/backoff.js
@@ -5,48 +5,68 @@ const assert = require('assert');
 const sinon = require('sinon');
 
 describe('Backoff', () => {
-  it('should call the function on start', () => {
-    const fn = sinon.spy();
-    const backoff = new DefaultBackoff(fn);
-    const fakeTimer = sinon.useFakeTimers();
+  describe('Function Call', () => {
+    let options = {};
+    let fn;
+    let backoff;
+    let fakeTimer;
+    beforeEach(() => {
+      fn = sinon.spy();
+      backoff = new DefaultBackoff(options);
+      fakeTimer = sinon.useFakeTimers();
+    });
 
-    backoff.start();
-    fakeTimer.tick(200);
-    sinon.assert.calledOnce(fn);
-    backoff.reset();
-    fakeTimer.restore();
+    afterEach(() => {
+      backoff.reset();
+      fakeTimer.restore();
+      fn.resetHistory();
+    });
+
+    it('should call the function on start', () => {
+      backoff.start(fn);
+      fakeTimer.tick(200);
+      sinon.assert.calledOnce(fn);
+    });
+
+    it('should increase the duration exponentially', () => {
+      backoff.start(fn);
+      fakeTimer.tick(110);
+      assert.strictEqual(100, backoff._duration);
+      backoff.start(fn);
+      fakeTimer.tick(210);
+      assert.strictEqual(200, backoff._duration);
+      backoff.start(fn);
+      fakeTimer.tick(410);
+      assert.strictEqual(400, backoff._duration);
+      sinon.assert.calledThrice(fn);
+    });
+
+    it('should reset the duration', () => {
+      backoff.start(fn);
+      fakeTimer.tick(110);
+      assert.strictEqual(100, backoff._duration);
+      backoff.reset();
+      assert.strictEqual(0, backoff._attempts);
+      assert.strictEqual(null, backoff._timeoutID);
+    });
   });
 
-  it('should increase the duration exponentially', () => {
-    const fn = sinon.spy();
-    const backoff = new DefaultBackoff(fn);
-    const fakeTimer = sinon.useFakeTimers();
+  describe('Options', () => {
+    it('should take in options', () => {
+      const options = {
+        min: 10,
+        max: 30000,
+        jitter: 1,
+        factor: 2
+      };
+      const backoff = new DefaultBackoff(options);
+      const fn = sinon.spy();
+      const fakeTimer = sinon.useFakeTimers();
 
-    backoff.start();
-    fakeTimer.tick(110);
-    assert.strictEqual(100, backoff._duration);
-    backoff.start();
-    fakeTimer.tick(210);
-    assert.strictEqual(200, backoff._duration);
-    backoff.start();
-    fakeTimer.tick(410);
-    assert.strictEqual(400, backoff._duration);
-    sinon.assert.calledThrice(fn);
-    backoff.reset();
-    fakeTimer.restore();
-  });
-
-  it('should reset the duration', () => {
-    const fn = sinon.spy();
-    const backoff = new DefaultBackoff(fn);
-    const fakeTimer = sinon.useFakeTimers();
-
-    backoff.start();
-    fakeTimer.tick(110);
-    assert.strictEqual(100, backoff._duration);
-    backoff.reset();
-    assert.strictEqual(0, backoff._attempts);
-    assert.strictEqual(null, backoff._timeoutID);
-    fakeTimer.restore();
+      backoff.start(fn);
+      fakeTimer.tick(20);
+      sinon.assert.calledOnce(fn);
+      backoff.reset();
+    });
   });
 });
