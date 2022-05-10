d89a2562b8679403af7ef614c925bed93594a303
Joyce Ma <joma@twilio.com>
2022-05-09 20:33:22 -0700

    Adding a few more unit tests

--- test/unit/spec/util/backoff.js
+++ test/unit/spec/util/backoff.js
@@ -5,7 +5,7 @@ const assert = require('assert');
 const sinon = require('sinon');
 
 describe('Backoff', () => {
-  it('should increase the backoff duration', () => {
+  it('should call the function on start', () => {
     const fn = sinon.spy();
     const backoff = new DefaultBackoff(fn);
     const fakeTimer = sinon.useFakeTimers();
@@ -14,6 +14,39 @@ describe('Backoff', () => {
     fakeTimer.tick(200);
     sinon.assert.calledOnce(fn);
     backoff.reset();
+    fakeTimer.restore();
+  });
+
+  it('should increase the duration exponentially', () => {
+    const fn = sinon.spy();
+    const backoff = new DefaultBackoff(fn);
+    const fakeTimer = sinon.useFakeTimers();
+
+    backoff.start();
+    fakeTimer.tick(110);
+    assert.strictEqual(100, backoff._duration);
+    backoff.start();
+    fakeTimer.tick(210);
+    assert.strictEqual(200, backoff._duration);
+    backoff.start();
+    fakeTimer.tick(410);
+    assert.strictEqual(400, backoff._duration);
+    sinon.assert.calledThrice(fn);
+    backoff.reset();
+    fakeTimer.restore();
+  });
+
+  it('should reset the duration', () => {
+    const fn = sinon.spy();
+    const backoff = new DefaultBackoff(fn);
+    const fakeTimer = sinon.useFakeTimers();
+
+    backoff.start();
+    fakeTimer.tick(110);
+    assert.strictEqual(100, backoff._duration);
+    backoff.reset();
+    assert.strictEqual(0, backoff._attempts);
+    assert.strictEqual(null, backoff._timeoutID);
     fakeTimer.restore();
   });
 });
