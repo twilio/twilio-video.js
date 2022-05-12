963dc18935cf593a5b54aa5c4f1f653e0b7df108
Joyce Ma <joma@twilio.com>
2022-05-12 16:19:38 -0700

    Removing previous change to documentvisibilitymonitor unit tests

--- test/unit/spec/util/documentvisibilitymonitor.js
+++ test/unit/spec/util/documentvisibilitymonitor.js
@@ -17,8 +17,8 @@ describe('DocumentVisibilityMonitor', () => {
   });
 
   after(() => {
-    addEventListenerStub.resetHistory();
-    removeEventListenerStub.resetHistory();
+    addEventListenerStub.restore();
+    removeEventListenerStub.restore();
     if (global.document instanceof Document) {
       delete global.document;
     }
