18512f964d75781d6cca12e43ca12c18df0abf58
Luis Rivas <lrivas@twilio.com>
2024-12-05 16:43:22 -0500

    docs: introduce beta support for Video Processor V3

--- tsdef/VideoProcessor.d.ts
+++ tsdef/VideoProcessor.d.ts
@@ -1,3 +1,6 @@
+/**
+ * @beta VideoFrame is part of Video Processor V3 support which is currently in beta.
+ */
 declare class VideoFrame {
   displayHeight: number;
   displayWidth: number;
@@ -6,7 +9,23 @@ declare class VideoFrame {
   close(): void;
 }
 
+/**
+ * A processor for manipulating video frames.
+ * @beta Video Processor V3 support is currently in beta.
+ */
 export class VideoProcessor {
+  /**
+   * Process a video frame.
+   * @param inputFrameBuffer The input frame buffer. Can be one of:
+   * - OffscreenCanvas
+   * - HTMLCanvasElement
+   * - HTMLVideoElement
+   * - VideoFrame (beta)
+   * @param outputFrameBuffer The output frame buffer. The context type can be:
+   * - '2d' (default)
+   * - 'bitmaprenderer' (beta, Chromium-based browsers only)
+   * @returns A Promise that resolves when the frame has been processed, or void for synchronous processing
+   */
   processFrame(
     inputFrameBuffer: OffscreenCanvas | HTMLCanvasElement | HTMLVideoElement | VideoFrame,
     outputFrameBuffer: HTMLCanvasElement
