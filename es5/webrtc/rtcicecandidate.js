25ca7173631aa1e81dc855c3105f1d63a4782831
twilio-ci <twilio-ci@twilio.com>
2025-03-26 16:46:54 +0000

    2.30.0-rc.3

---
+++ es5/webrtc/rtcicecandidate.js
@@ -0,0 +1,11 @@
+/* global RTCIceCandidate */
+'use strict';
+if (typeof RTCIceCandidate === 'function') {
+    module.exports = RTCIceCandidate;
+}
+else {
+    module.exports = function RTCIceCandidate() {
+        throw new Error('RTCIceCandidate is not supported');
+    };
+}
+//# sourceMappingURL=rtcicecandidate.js.map
