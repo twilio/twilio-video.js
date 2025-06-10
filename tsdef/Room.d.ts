f80ac756b4746a08fe8139f9c7c0a02f01ab4bfa
Luis Rivas <lrivas@twilio.com>
2025-06-10 18:35:10 -0500

    [VBLOCKS-4773] prepare RC for real-time transcriptions (#2108)
    * Live transcription APIs (#2102)
    
    * tests: add unit tests for LiveTranscriptionSignaling
    
    * tests: add e2e tests for live transcriptions
    
    * test: add unit tests for enableLiveTranscription flag in TwilioConnectionTransport
    
    * doc: enhance JSDoc and types for live transcription events
    
    * types: add enableLiveTranscription option to connection options
    
    * chore: update CHANGELOG for live transcription feature
    
    ---------
    
    Co-authored-by: Charlie Santos <csantos@twilio.com>

--- tsdef/Room.d.ts
+++ tsdef/Room.d.ts
@@ -1,4 +1,4 @@
-import { LocalTrack, RemoteTrack, StatsReport } from './types';
+import { RemoteTrack, StatsReport, TranscriptionEvent } from './types';
 import { EventEmitter } from 'events';
 import { LocalParticipant } from './LocalParticipant';
 import { LocalTrackPublication } from './LocalTrackPublication';
@@ -30,6 +30,7 @@ export class Room extends EventEmitter {
 
   on(event: 'disconnected', listener: (room: Room, error: TwilioError) => void): this;
   on(event: 'dominantSpeakerChanged', listener: (dominantSpeaker: RemoteParticipant) => void): this;
+  on(event: 'liveTranscription', listener: (transcriptionEvent: TranscriptionEvent) => void): this;
   on(event: 'participantConnected', listener: (participant: RemoteParticipant) => void): this;
   on(event: 'participantDisconnected', listener: (participant: RemoteParticipant) => void): this;
   on(event: 'participantReconnected', listener: (participant: RemoteParticipant) => void): this;
