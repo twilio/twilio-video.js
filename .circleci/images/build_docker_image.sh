ec8899e316e7b87a984c0a6fbf13150fc3daa631
Kevin Choy <kevinchoy22@gmail.com>
2026-01-22 16:16:25 -0800

    ci: push docker image

--- .circleci/images/build_docker_image.sh
+++ .circleci/images/build_docker_image.sh
@@ -8,38 +8,15 @@ echo $PWD
 
 mkdir -p ./logs
 
-# first get the version of current image.
-echo "Checking Current version for ${BROWSER}-${BVER}"
-# first run ensures that we do not get output from docker pull
-docker-compose --file=.circleci/images/docker-compose.yml run --rm getVersion
-OLD_VERSION=$(docker-compose --file=.circleci/images/docker-compose.yml run --rm getVersion)
-echo "========================================================="
-echo "Found old version for ${BROWSER}-${BVER} = ${OLD_VERSION}"
-echo "========================================================="
-echo ${OLD_VERSION} > ./logs/oldversion.txt
-
 echo "Building new image for ${BROWSER}-${BVER}"
 docker-compose --file=.circleci/images/docker-compose.yml build browserContainer
 
-echo "Checking New version for ${BROWSER}-${BVER}"
-# first run ensures that we do not get output from docker pull
-docker-compose --file=.circleci/images/docker-compose.yml run --rm getVersion
+echo "Getting version for ${BROWSER}-${BVER}"
 NEW_VERSION=$(docker-compose --file=.circleci/images/docker-compose.yml run --rm getVersion)
 echo "========================================================="
-echo "Found new version for ${BROWSER}-${BVER} = ${NEW_VERSION}"
+echo "Found version for ${BROWSER}-${BVER} = ${NEW_VERSION}"
 echo "========================================================="
 echo ${NEW_VERSION} > ./logs/newversion.txt
-
-if [ "${NEW_VERSION}" == "${OLD_VERSION}" ]; then
-    echo "========================================================="
-    echo "No version change detected. Exiting"
-    echo "========================================================="
-    echo notpushed > ./logs/notpushed.txt
-    SLACK_MESSAGE_TEXT="${BROWSER}-${BVER} is ${NEW_VERSION}"
-    CLEANEDUP_SLACK_MESSAGE_TEXT=$(echo $SLACK_MESSAGE_TEXT|tr -d '\n\r\t')
-    curl -X POST -H 'Content-type: application/json' --data '{"text": '\""$CLEANEDUP_SLACK_MESSAGE_TEXT"\"'}' $SLACK_WEBHOOK
-    exit 0
-fi
 
 # push newly generated image
 if [ "${CIRCLECI}" == "true" ]; then
